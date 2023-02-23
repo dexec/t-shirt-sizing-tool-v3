6f911e91a4a2be106a6a7745973aadfa5300139a
Daniel Ackermann <d.ackermann@smf.de>
2023-02-23 08:43:57 +0100

    Neue Getter und Mutations hinzugefügt

--- src/store/index.js
+++ src/store/index.js
@@ -1,22 +1,29 @@
 import {createStore} from 'vuex'
 import saveFile from './file.json'
-import Paket from '../Paket.js'
+/*
+
+var hashmap = new Object();
+hashmap.meinAttribut = "Hallo Welt";
+hashmap["123"] = "Hallo Welt";
+hashmap["Mein tolles Attribut"] = "Hallo Welt";
+
+hashmap.meinAttribut
+hashmap.123
+hashmap.Mein tolles Attribut
+
+
+
+for(paket : pakete)
+{
+    hashmap[paket.id] = paket;
+}
+*/
 
 export const store = createStore({
     state() {
         return {
             buckets: saveFile.buckets,
-            pakete: saveFile.pakete,
-            testBuckets: [
-                {
-                    id: 0,
-                    name: 'XXL'
-                },
-                {
-                    id: 1,
-                    name: 'abc'
-                }
-            ]
+            pakete: saveFile.pakete
         }
     },
     getters: {
@@ -24,19 +31,41 @@ export const store = createStore({
             const result = [];
             const stack = [];
             for (let paket of state.pakete) {
-                stack.push(paket)
+                paket.lvl = 0;
+                stack.push(paket);
             }
+
             while (stack.length > 0) {
-                const aktuellesPaket = stack.pop();
-                const neuesPaket = new Paket(aktuellesPaket.id, aktuellesPaket.ticket_nr, aktuellesPaket.thema, aktuellesPaket.beschreibung, aktuellesPaket.komponente, aktuellesPaket.bucket, aktuellesPaket.schaetzung, aktuellesPaket.children)
+                const aktuellesPaket = stack.shift();
+                //const neuesPaket = new Paket(aktuellesPaket.id, aktuellesPaket.ticket_nr, aktuellesPaket.thema, aktuellesPaket.beschreibung, aktuellesPaket.komponente, aktuellesPaket.bucket, aktuellesPaket.schaetzung, aktuellesPaket.visible, aktuellesPaket.open, aktuellesPaket.children)
                 if (aktuellesPaket.children.length > 0) {
-                    for (let paketChild of aktuellesPaket.children)
-                        stack.push(paketChild)
+                    //aktuellesPaket.children.reverse()
+                    for (let i = aktuellesPaket.children.length - 1; i >= 0; i--) {
+                        aktuellesPaket.children[i].lvl = aktuellesPaket.lvl + 1;
+                        stack.unshift(aktuellesPaket.children[i]);
+                    }
                 }
-                result.push(neuesPaket)
+                result.push(aktuellesPaket)
             }
-            console.log("debug")
             return result
+        },
+        asTree(state) {
+            return state.pakete
+        },
+        paketWithId: (state) => (id) => {
+            const stack = state.pakete;
+            while (stack.length > 0) {
+                const aktuellesPaket = stack.shift();
+                if (aktuellesPaket.id === id) return aktuellesPaket;
+                //const neuesPaket = new Paket(aktuellesPaket.id, aktuellesPaket.ticket_nr, aktuellesPaket.thema, aktuellesPaket.beschreibung, aktuellesPaket.komponente, aktuellesPaket.bucket, aktuellesPaket.schaetzung, aktuellesPaket.visible, aktuellesPaket.open, aktuellesPaket.children)
+                if (aktuellesPaket.children.length > 0) {
+                    //aktuellesPaket.children.reverse()
+                    for (let child of aktuellesPaket.children) {
+                        stack.unshift(child);
+                    }
+                }
+            }
+            return -1;
         }
     },
     mutations: {
@@ -48,8 +77,34 @@ export const store = createStore({
                 }
             }*/
         },
-        updateTestBucket(state, name) {
-            state.testBuckets[0].name = name
+        updatePaket(state, newPaket) {
+            const stack = state.pakete;
+            while (stack.length > 0) {
+                let aktuellesPaket = stack.shift();
+                if (aktuellesPaket.id === newPaket.id) {
+                    aktuellesPaket = newPaket;
+                    return;
+                }
+                //const neuesPaket = new Paket(aktuellesPaket.id, aktuellesPaket.ticket_nr, aktuellesPaket.thema, aktuellesPaket.beschreibung, aktuellesPaket.komponente, aktuellesPaket.bucket, aktuellesPaket.schaetzung, aktuellesPaket.visible, aktuellesPaket.open, aktuellesPaket.children)
+                if (aktuellesPaket.children.length > 0) {
+                    //aktuellesPaket.children.reverse()
+                    for (let child of aktuellesPaket.children) {
+                        stack.unshift(child);
+                    }
+                }
+            }
+        },
+        unterElementeInvisibleSchalten(state, wurzel) {
+            const stack = wurzel.children;
+            while (stack.length > 0) {
+                const aktuellesPaket = stack.shift();
+                aktuellesPaket.visible = false
+                if (aktuellesPaket.children.length > 0) {
+                    for (let child of aktuellesPaket.children) {
+                        stack.unshift(child);
+                    }
+                }
+            }
         }
     },
     actions: {},
