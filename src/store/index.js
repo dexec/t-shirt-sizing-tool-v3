5de9d03bf3bf801ccc25aeb6f49113e77f0b9a51
Daniel Ackermann <d.ackermann@smf.de>
2023-02-28 13:23:26 +0100

    Pakete werden nun als Treelist eingelesen und hierarchisch sortiert, deletePaket hinzugefügt

--- src/store/index.js
+++ src/store/index.js
@@ -1,109 +1,75 @@
 import {createStore} from 'vuex'
 import saveFile from './file.json'
-/*
 
-var hashmap = new Object();
-hashmap.meinAttribut = "Hallo Welt";
-hashmap["123"] = "Hallo Welt";
-hashmap["Mein tolles Attribut"] = "Hallo Welt";
-
-hashmap.meinAttribut
-hashmap.123
-hashmap.Mein tolles Attribut
-
-
-
-for(paket : pakete)
-{
-    hashmap[paket.id] = paket;
+function loadData() {
+    const paketeAsList = [];
+    const stack = JSON.parse(JSON.stringify(saveFile.pakete));
+    let idCounter = 0;
+    for (let paket of stack) {
+        paket.parent = null;
+        paket.lvl = 0;
+    }
+    while (stack.length > 0) {
+        const aktuellesPaket = stack.shift();
+        aktuellesPaket.id = idCounter++;
+        if (aktuellesPaket.children.length > 0) {
+            for (let i = aktuellesPaket.children.length - 1; i >= 0; i--) {
+                aktuellesPaket.children[i].lvl = aktuellesPaket.lvl + 1;
+                aktuellesPaket.children[i].parent = aktuellesPaket;
+                stack.unshift(aktuellesPaket.children[i]);
+            }
+        }
+        paketeAsList.push(aktuellesPaket)
+    }
+    return paketeAsList
 }
-*/
+
 
 export const store = createStore({
     state() {
         return {
             buckets: saveFile.buckets,
-            pakete: saveFile.pakete
+            paketeAsList: loadData(),
         }
     },
-    getters: {
-        asList(state) {
-            const result = [];
-            const stack = [];
-            for (let paket of state.pakete) {
-                paket.lvl = 0;
-                stack.push(paket);
-            }
-
-            while (stack.length > 0) {
-                const aktuellesPaket = stack.shift();
-                //const neuesPaket = new Paket(aktuellesPaket.id, aktuellesPaket.ticket_nr, aktuellesPaket.thema, aktuellesPaket.beschreibung, aktuellesPaket.komponente, aktuellesPaket.bucket, aktuellesPaket.schaetzung, aktuellesPaket.visible, aktuellesPaket.open, aktuellesPaket.children)
-                if (aktuellesPaket.children.length > 0) {
-                    //aktuellesPaket.children.reverse()
-                    for (let i = aktuellesPaket.children.length - 1; i >= 0; i--) {
-                        aktuellesPaket.children[i].lvl = aktuellesPaket.lvl + 1;
-                        stack.unshift(aktuellesPaket.children[i]);
-                    }
-                }
-                result.push(aktuellesPaket)
-            }
-            return result
-        },
-        asTree(state) {
-            return state.pakete
-        },
-        paketWithId: (state) => (id) => {
-            const stack = state.pakete;
-            while (stack.length > 0) {
-                const aktuellesPaket = stack.shift();
-                if (aktuellesPaket.id === id) return aktuellesPaket;
-                //const neuesPaket = new Paket(aktuellesPaket.id, aktuellesPaket.ticket_nr, aktuellesPaket.thema, aktuellesPaket.beschreibung, aktuellesPaket.komponente, aktuellesPaket.bucket, aktuellesPaket.schaetzung, aktuellesPaket.visible, aktuellesPaket.open, aktuellesPaket.children)
-                if (aktuellesPaket.children.length > 0) {
-                    //aktuellesPaket.children.reverse()
-                    for (let child of aktuellesPaket.children) {
-                        stack.unshift(child);
-                    }
-                }
-            }
-            return -1;
-        }
-    },
+    getters: {},
     mutations: {
         updateAllBuckets(state, newBucketArray) {
             state.buckets = newBucketArray
-            /*for (let oldBucket in state.buckets) {
-                if (oldBucket.id === bucket.id) {
-                    state.buckets[state.buckets.indexOf(oldBucket)] = bucket
-                }
-            }*/
         },
         updatePaket(state, newPaket) {
-            const stack = state.pakete;
-            while (stack.length > 0) {
-                let aktuellesPaket = stack.shift();
-                if (aktuellesPaket.id === newPaket.id) {
-                    aktuellesPaket = newPaket;
-                    return;
-                }
-                //const neuesPaket = new Paket(aktuellesPaket.id, aktuellesPaket.ticket_nr, aktuellesPaket.thema, aktuellesPaket.beschreibung, aktuellesPaket.komponente, aktuellesPaket.bucket, aktuellesPaket.schaetzung, aktuellesPaket.visible, aktuellesPaket.open, aktuellesPaket.children)
-                if (aktuellesPaket.children.length > 0) {
-                    //aktuellesPaket.children.reverse()
-                    for (let child of aktuellesPaket.children) {
-                        stack.unshift(child);
+            let oldPaket = state.paketeAsList.find(paket => paket.id === newPaket.id);
+            if (typeof oldPaket !== 'undefined') oldPaket = newPaket
+        },
+        updateUnterPakete(state, wurzel) {
+            const wurzelPaket = state.paketeAsList.find(paket => paket.id === wurzel.id);
+            if (typeof wurzelPaket !== 'undefined') {
+                const stack = [];
+                stack.push(wurzelPaket)
+                while (stack.length > 0) {
+                    const aktuellesPaket = stack.shift();
+                    if (aktuellesPaket.children.length > 0) {
+                        for (let child of aktuellesPaket.children) {
+                            wurzelPaket.open ? child.visible = aktuellesPaket.open : child.visible = wurzelPaket.open;
+                            stack.unshift(child);
+                        }
                     }
                 }
             }
         },
-        unterElementeInvisibleSchalten(state, wurzel) {
-            const stack = wurzel.children;
+        deletePaket(state, id) {
+            const stack = [];
+            const paketToDelete = state.paketeAsList[id];
+            stack.push(paketToDelete)
             while (stack.length > 0) {
                 const aktuellesPaket = stack.shift();
-                aktuellesPaket.visible = false
-                if (aktuellesPaket.children.length > 0) {
-                    for (let child of aktuellesPaket.children) {
-                        stack.unshift(child);
-                    }
+                state.paketeAsList = state.paketeAsList.filter(paket => paket.id !== aktuellesPaket.id)
+                for (let i = 0; i < aktuellesPaket.children.length; i++) {
+                    stack.push(aktuellesPaket.children[i]);
                 }
+            }
+            for (let i = 0; i < state.paketeAsList.length > 0; i++) {
+                state.paketeAsList[i].id = i;
             }
         }
     },
