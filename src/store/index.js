cf4b6a68b3bc20fdf228338d1d845ff5c8dba4c7
Daniel Ackermann <d.ackermann@smf.de>
2023-03-07 21:13:58 +0100

    Map hinzugefügt, Logik verändert

--- src/store/index.js
+++ src/store/index.js
@@ -1,76 +1,102 @@
 import {createStore} from 'vuex'
 import saveFile from './file.json'
 
-function loadData() {
-    const stack = JSON.parse(JSON.stringify(saveFile.pakete));
-    const result = [];
-    for (let paket of stack) {
-        paket.parent = null;
-        paket.lvl = 0;
-    }
-    while (stack.length > 0) {
-        const aktuellesPaket = stack.shift();
+const stackForMap = JSON.parse(JSON.stringify(saveFile.pakete));
+const stackForTreeView = [];
+let idCounter = 0;
+
+const map = new Map();
+for (let paket of stackForMap) {
+    paket.parent = null;
+    paket.lvl = 0;
+    stackForTreeView.push(paket)
+}
+while (stackForMap.length > 0) {
+    const aktuellesPaket = stackForMap.shift();
+    if (aktuellesPaket.children.length > 0) {
         for (let child of aktuellesPaket.children) {
             child.lvl = aktuellesPaket.lvl + 1;
             child.parent = aktuellesPaket;
-            stack.push(child);
+            stackForMap.push(child);
         }
-        result.push(aktuellesPaket)
     }
-    for (let i = 0; i < 1000; i++) {
-        result.push({
-            id: i,
-            ticket_nr: 111,
-            thema: "Testticket",
-            beschreibung: "Ticket zum Stresstesten",
-            komponente: "Test",
-            bucket: null,
-            schaetzung: null,
-            visible: null,
-            open: false,
-            parent: null,
-            children: []
-        })
+    aktuellesPaket.id = idCounter++;
+    map.set(aktuellesPaket.id, aktuellesPaket);
+}
+
+const pakateAsTreeView = [];
+while (stackForTreeView.length > 0) {
+    const aktuellesPaket = stackForTreeView.shift();
+    if (aktuellesPaket.children.length > 0) {
+        for (let i = aktuellesPaket.children.length - 1; i >= 0; i--) {
+            if (aktuellesPaket.open)
+                stackForTreeView.unshift(aktuellesPaket.children[i]);
+        }
     }
-    return result
+    pakateAsTreeView.push(aktuellesPaket)
+}
+
+for (let i = 0; i < 1000; i++) {
+    pakateAsTreeView.push({
+        id: i,
+        ticket_nr: 111,
+        thema: "Testticket",
+        beschreibung: "Ticket zum Stresstesten",
+        komponente: "Test",
+        bucket: null,
+        schaetzung: null,
+        visible: null,
+        open: false,
+        parent: null,
+        children: []
+    })
 }
 
 export const store = createStore({
     state() {
         return {
             buckets: saveFile.buckets,
-            paketeAsList: loadData(),
-            paketeAsSortedList: () => { return []; }
+            paketeAsMap: map,
+            paketeAsTreeView: pakateAsTreeView
         }
     },
-    getters: {
-        paketeAsSortedList(state) {
-            console.log("getter")
-            let idCounter = 0;
-            const result = [];
-            const stack = state.paketeAsList.filter(paket => paket.parent === null);
-            while (stack.length > 0) {
-                const aktuellesPaket = stack.shift();
-                if (aktuellesPaket.open && aktuellesPaket.children.length > 0) {
-                    for (let i = aktuellesPaket.children.length - 1; i >= 0; i--) {
-                        aktuellesPaket.children[i].lvl = aktuellesPaket.lvl + 1;
-                        aktuellesPaket.children[i].parent = aktuellesPaket;
-                        stack.unshift(aktuellesPaket.children[i]);
-                    }
-                }
-                aktuellesPaket.id = idCounter++;
-                result.push(aktuellesPaket)
-            }
-            return result
-        }
-    },
+    getters: {},
     mutations: {
         updateAllBuckets(state, newBucketArray) {
             state.buckets = newBucketArray
         },
         updatePaket(state, newPaket) {
-            let oldPaket = state.paketeAsList[newPaket.id]
+            let oldPaket = state.paketeAsMap.get(newPaket.id)
             if (typeof oldPaket !== 'undefined') oldPaket = newPaket
+        },
+        changeOpenState(state, changedPaket) {
+            const indexOfChangedPaket = state.paketeAsTreeView.indexOf(changedPaket)
+            changedPaket.open = !changedPaket.open;
+            let counter = 0;
+            const stack = [...changedPaket.children];
+            if (changedPaket.open) {
+                while (stack.length > 0) {
+                    const aktuellesPaket = stack.shift();
+                    if (aktuellesPaket.children.length > 0) {
+                        for (let i = aktuellesPaket.children.length - 1; i >= 0; i--) {
+                            if (aktuellesPaket.open) stack.unshift(aktuellesPaket.children[i]);
+                        }
+                    }
+                    state.paketeAsTreeView.splice(indexOfChangedPaket + 1 + counter++, 0, aktuellesPaket)
+                }
+            }
+            else {
+                while(stack.length > 0) {
+                    const aktuellesPaket = stack.shift();
+                    if (aktuellesPaket.children.length > 0) {
+                        for (let child of aktuellesPaket.children) {
+                            if (aktuellesPaket.open) stack.unshift(child);
+                        }
+                    }
+                    counter++;
+                }
+                state.paketeAsTreeView.splice(indexOfChangedPaket+1,counter)
+            }
         },
         deletePaket(state, id) {
             const paketToDelete = state.paketeAsList.find(paket => paket.id === id);
@@ -102,7 +128,7 @@ export const store = createStore({
                 children: []
             }
             parentOfNewPaket.children.unshift(newPaket);
-            state.paketeAsList.push(newPaket)
+            state.paketeAsList.push(newPaket);
         }
     },
     actions: {},
