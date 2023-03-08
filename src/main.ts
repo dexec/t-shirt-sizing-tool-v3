09ce9b839e7f4f35c29f566903562cbec24e2400
Daniel Ackermann <d.ackermann@smf.de>
2023-03-08 17:06:16 +0100

    Pinia und TS hinzugefügt

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
-import {createApp} from 'vue'
+import { createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
-import {store} from './store'
+import { createPinia } from 'pinia'
 
 // Vuetify
 import 'vuetify/styles'
@@ -10,12 +10,15 @@ import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 import '@mdi/font/css/materialdesignicons.css'
 
+
 const vuetify = createVuetify({
     components,
     directives,
 })
+
+const pinia = createPinia()
 const app = createApp(App)
+app.use(pinia)
 app.use(router)
 app.use(vuetify)
-app.use(store)
 app.mount('#app')
