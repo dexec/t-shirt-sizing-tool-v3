c06f478b047467eb2e67ed600e8fce6bf58e6cff
Daniel <daniel.a.m@web.de>
2023-03-09 12:47:16 +0100

    Neue Pakete hinzufügen angepasst, nextTick verwendet

--- src/views/PaketUebersichtView.vue
+++ src/views/PaketUebersichtView.vue
@@ -1,18 +1,20 @@
 <template>
-  <div style='width: 100%;height: 100%'>
+  <div style="width: 100%;height: 100%">
     <ag-grid-vue
-      style='width: 100%;height: 90%'
-      class='ag-theme-alpine'
-      :rowData='rowData'
-      :columnDefs='columnDefs'
-      :defaultColDef='defaultColDef'
-      suppressRowHoverHighlight='true'
-      @grid-ready='onGridReady'>
+      style="width: 100%;height: 90%"
+      class="ag-theme-alpine"
+      :rowData="rowData"
+      :columnDefs="columnDefs"
+      :defaultColDef="defaultColDef"
+      suppressRowHoverHighlight="true"
+      @cellClicked="onCellClicked"
+      @grid-ready="onGridReady">
     </ag-grid-vue>
-    <v-card class='d-flex justify-center fixed'>
-      <v-btn :disabled='this.selectedRow === null' class='mx-5' @click='addNewPaket'>Neues Arbeitspaket anlegen</v-btn>
-      <v-btn :disabled='this.selectedRow === null' class='mx-5' @click='removeItem'>Arbeitspaket löschen</v-btn>
-      <v-btn :disabled='this.selectedRow === null' class='mx-5'>Bucket zuweisen</v-btn>
+    <v-card class="d-flex justify-center fixed">
+      <v-btn class="mx-5" @click="addNewPaket">Neues Arbeitspaket anlegen</v-btn>
+      <v-btn :disabled="this.selectedRow === null" class="mx-5" @click="addNewKindPaket">Neues Arbeitspaket als Kind anlegen</v-btn>
+      <v-btn :disabled="this.selectedRow === null" class="mx-5" @click="removeItem">Arbeitspaket löschen</v-btn>
+      <v-btn :disabled="this.selectedRow === null" class="mx-5">Bucket zuweisen</v-btn>
     </v-card>
   </div>
 </template>
@@ -80,15 +82,16 @@ function removeItem() {
 }
 </script>-->
 <script>
-import 'ag-grid-community/styles/ag-grid.css'
-import 'ag-grid-community/styles/ag-theme-alpine.css'
-import { AgGridVue } from 'ag-grid-vue3'
-import TreeDataCellRenderer from '@/components/TreeDataCellRenderer.vue'
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-alpine.css";
+import { AgGridVue } from "ag-grid-vue3";
+import TreeDataCellRenderer from "@/components/TreeDataCellRenderer.vue";
 
-import { usePaketeStore } from '@/stores/pakete'
+import { usePaketeStore } from "@/stores/pakete";
+import { nextTick } from "vue";
 
 export default {
-  name: 'PaketUebersichtView',
+  name: "PaketUebersichtView",
   data() {
     return {
       gridApi: null,
@@ -99,44 +102,40 @@ export default {
       },
       columnDefs: [
         {
-          field: 'ticket_nr',
-          headerName: 'Ticket-NR',
+          field: "ticket_nr",
+          headerName: "Ticket-NR",
           minWidth: 5,
           cellRenderer: TreeDataCellRenderer
         },
         {
-          field: 'thema',
-          headerName: 'Thema'
+          field: "thema",
+          headerName: "Thema"
         },
         {
-          field: 'beschreibung',
-          headerName: 'Beschreibung'
+          field: "beschreibung",
+          headerName: "Beschreibung"
         },
         {
-          field: 'komponente',
-          headerName: 'Komponente'
+          field: "komponente",
+          headerName: "Komponente"
         },
         {
-          field: 'bucket',
-          headerName: 'Bucket',
+          field: "bucket",
+          headerName: "Bucket",
           editable: false
         },
         {
-          field: 'schaetzung',
-          headerName: 'Schätzung'
+          field: "schaetzung",
+          headerName: "Schätzung"
         }
       ]
 
-    }
+    };
   },
   setup() {
-    const paketeStore = usePaketeStore()
-    return {paketeStore}
-  },
-  computed: {
-    rowData() {
-      return this.paketeStore.getTreeView
-    }
+    const paketeStore = usePaketeStore();
+    const rowData = paketeStore.paketeAsTreeView;
+    return { rowData, paketeStore };
   },
   components: {
     AgGridVue,
@@ -149,23 +148,30 @@ export default {
           params.columnApi.autoSizeColumns()
         },*/
     onGridReady(params) {
-      this.gridApi = params.api
-      this.columnApi = params.columnApi
+      this.gridApi = params.api;
+      this.columnApi = params.columnApi;
       //this.gridApi.getDisplayedRowAtIndex(0).setSelected(true)
     },
     onCellClicked(params) {
-      this.selectedRow = params.data
+      this.selectedRow = params.data;
     },
     addNewPaket() {
-      this.paketeStore.addNew(this.selectedRow)
+      this.paketeStore.addNew(null);
+      nextTick(() => this.gridApi.setRowData(this.paketeStore.getTreeView));
+    },
+    addNewKindPaket() {
+      this.paketeStore.addNew(this.selectedRow);
+      nextTick(() => this.gridApi.setRowData(this.paketeStore.getTreeView));
     },
     removeItem() {
-      if (typeof this.selectedRow !== 'undefined') {
-        this.paketeStore.deletePaket(this.selectedRow.id)
+      if (this.selectedRow!=null) {
+        this.paketeStore.deletePaket(this.selectedRow.id);
+        nextTick(() => this.gridApi.setRowData(this.paketeStore.getTreeView));
       }
+      this.selectedRow=null
     }
   }
-}
+};
 
 </script>
 
