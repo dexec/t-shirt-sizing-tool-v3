07c5d562de85d08dc0fe5587f59c72e0cda595c8
Daniel <daniel.a.m@web.de>
2023-03-09 12:48:00 +0100

    testing

--- src/views/testView.vue
+++ src/views/testView.vue
@@ -1,12 +1,7 @@
 <template>
-  <div>{{ store.getBuckets }}</div>
-  <div>{{ paketeStore.$state.paketeAsTreeView }}</div>
+<div></div>
 </template>
 
 <script setup>
-import { useBucketsStore } from '@/stores/bucket'
-import {usePaketeStore} from '@/stores/pakete'
 
-const paketeStore = usePaketeStore();
-const store = useBucketsStore();
 </script>
