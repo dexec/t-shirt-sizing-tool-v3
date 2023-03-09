008fea47e8421307ddfe0d725557f6eba673099c
Daniel <daniel.a.m@web.de>
2023-03-09 12:46:20 +0100

    Pakete um parent erweitert

--- src/Paket.ts
+++ src/Paket.ts
@@ -3,25 +3,25 @@ export class Paket {
   ticket_nr: string | null
   thema: string | null
   beschreibung: string | null
-  komponente: string| null
-  bucket: string| null
-  schaetzung: number| null
-  open: boolean| null
+  komponente: string | null
+  bucket: string | null
+  schaetzung: number | null
+  open: boolean | null
   lvl: number
+  parent?: Paket | null
   children: Paket[]
 
-
-  constructor(id: number, ticket_nr: string | null, thema: string | null, beschreibung: string | null, komponente: string | null, bucket: string | null, schaetzung: number | null, open: boolean | null, lvl: number, children: Paket[]) {
-    this.id = id
-    this.ticket_nr = ticket_nr
-    this.thema = thema
-    this.beschreibung = beschreibung
-    this.komponente = komponente
-    this.bucket = bucket
-    this.schaetzung = schaetzung
-    this.open = open
-    this.lvl = lvl
-    this.children = children
+  constructor(id: number, ticket_nr: string | null, thema: string | null, beschreibung: string | null, komponente: string | null, bucket: string | null, schaetzung: number | null, open: boolean | null, lvl: number, parent: Paket | null, children: Paket[]) {
+    this.id = id;
+    this.ticket_nr = ticket_nr;
+    this.thema = thema;
+    this.beschreibung = beschreibung;
+    this.komponente = komponente;
+    this.bucket = bucket;
+    this.schaetzung = schaetzung;
+    this.open = open;
+    this.lvl = lvl;
+    this.parent = parent;
+    this.children = children;
   }
 }
-
