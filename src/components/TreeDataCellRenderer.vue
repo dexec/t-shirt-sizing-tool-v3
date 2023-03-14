4ad38d97d8853d6e63f872d03b8c7ec1cb6fc1e7
Daniel Ackermann <d.ackermann@smf.de>
2023-03-14 11:55:08 +0100

    stuff zu params umbenannt

--- src/components/TreeDataCellRenderer.vue
+++ src/components/TreeDataCellRenderer.vue
@@ -1,32 +1,32 @@
 <template>
-  <div :style="'margin-left:' + stuff.node.data.lvl * 20 + 'px'">
-    <v-btn size="x-small" :style="stuff.node.data.children.length>0 ? 'visibility:visible':'visibility:hidden'"
+  <div :style="'margin-left:' + params.node.data.lvl * 20 + 'px'">
+    <v-btn size="x-small" :style="params.node.data.children.length>0 ? 'visibility:visible':'visibility:hidden'"
            @click="changeOpenState">
-      <v-icon size="x-small" v-if="!stuff.node.data.open">mdi-plus</v-icon>
+      <v-icon size="x-small" v-if="!params.node.data.open">mdi-plus</v-icon>
       <v-icon size="x-small" v-else>mdi-minus</v-icon>
     </v-btn>
-    {{ stuff.value }}
+    {{ params.value }}
   </div>
 </template>
 
 <script>
 
 import {nextTick} from "vue";
-import { usePaketeStore } from '@/stores/pakete'
+import {usePaketeStore} from '@/stores/pakete'
 
 export default {
   name: "TestComp",
   setup(props) {
-    return { stuff: props.params };
+    return {params: props.params};
   },
   methods: {
     changeOpenState() {
       const paketeStore = usePaketeStore();
-      let aktuellesPaket = this.stuff.node.data;
+      let aktuellesPaket = this.params.node.data;
       aktuellesPaket.open = !aktuellesPaket.open;
-      this.stuff.node.setData(aktuellesPaket);
-      paketeStore.updateTreeView(aktuellesPaket);
-      nextTick(() => this.stuff.api.setRowData(paketeStore.getTreeView))
+      this.params.node.setData(aktuellesPaket);
+      paketeStore.updateTreeViewAfterChangedOpenState(aktuellesPaket);
+      nextTick(() => this.params.api.setRowData(paketeStore.getTreeView))
     }
   }
 }
