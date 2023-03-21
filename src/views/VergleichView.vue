6fa83d25c6135871788178480cda7ceb5e10ca05
Daniel Ackermann <d.ackermann@smf.de>
2023-03-21 12:47:35 +0100

    Stores eingesetzt, Checkboxen sind jetzt als Drawer umgesetzt

--- src/views/VergleichView.vue
+++ src/views/VergleichView.vue
@@ -1,36 +1,23 @@
 <template>
   <v-container fluid class="fill-height">
     <v-row class="fill-height">
-      <v-col align-self="center" class="justify-start" cols="1">
-<!--        <v-btn
-            color="pink"
-            @click.stop="drawer = !drawer"
-        >
-          Toggle
-        </v-btn>-->
-        <v-navigation-drawer
-            v-model="drawer"
-            permanent
-        >
-          <v-checkbox v-for="bucket in buckets" :key="bucket.id" v-model="selected" :value="bucket.id"
-                      :label=bucket.name @change="sortSelectedBuckets">
-          </v-checkbox>
-        </v-navigation-drawer>
-      </v-col>
+      <!--        <v-btn
+                  color="pink"
+                  @click.stop="drawer = !drawer"
+              >
+                Toggle
+              </v-btn>-->
+      <!--        <v-navigation-drawer
+                  v-model="drawer"
+              >
+                <v-checkbox v-for="bucket in buckets" :key="bucket.id" v-model="selected" :value="bucket.id"
+                            :label=bucket.name @change="sortSelectedBuckets">
+                </v-checkbox>
+              </v-navigation-drawer>-->
       <v-col cols="9" class="d-flex flex-column">
         <v-row justify="center">
-          <v-btn
-              class="ma-2"
-              color="primary"
-              dark
-          >
-            Accept
-            <v-icon
-                dark
-                right
-            >
-              mdi-checkbox-marked-circle
-            </v-icon>
+          <v-btn class="ma-2" color="primary" dark>
+            <v-icon dark right>mdi-arrow-left</v-icon>
           </v-btn>
           <v-table style="width: 30%;">
             <tr>
@@ -42,82 +29,84 @@
               <td>Thema 1</td>
             </tr>
           </v-table>
-          <v-btn
-              class="ma-2"
-              color="primary"
-              dark
-          >
-            Accept
-            <v-icon
-                dark
-                right
-            >
-              mdi-checkbox-marked-circle
-            </v-icon>
+          <v-btn class="ma-2" color="primary" dark>
+            <v-icon dark right>mdi-arrow-right</v-icon>
           </v-btn>
         </v-row>
         <v-row class="d-flex flex-nowrap">
           <v-table v-for="selectedBuckedId in selected" :key="selected.indexOf(selectedBuckedId)" class="mr-4"
-                          style="width: 30%">
+                   style="width: 30%">
             <tr>
               <th colspan="2">
                 {{ buckets.find(bucket => bucket.id === selectedBuckedId).name }}
               </th>
             </tr>
-            <tr v-for="ticket in tickets.filter(ticket => ticket.bucket===buckets.find(bucket => bucket.id === selectedBuckedId).name)"
-                :key="ticket.id">
-              <td>{{ ticket.id }}</td>
-              <td>{{ ticket.thema }}</td>
+            <tr v-for="paket in pakete.filter(currentPaket => currentPaket.bucket===buckets.find(bucket => bucket.id === selectedBuckedId).name)"
+                :key="paket.id">
+              <td style="width: 10%">{{ paket.id }}</td>
+              <td>{{ paket.thema }}</td>
             </tr>
           </v-table>
         </v-row>
       </v-col>
-      <v-col cols="2">
-<!--          <v-btn
-              color="pink"
-              dark
-              @click.stop="drawer2 = !drawer2"
-          >
-            Toggle
-          </v-btn>-->
-        <v-navigation-drawer
-            v-model="drawer2"
-            permanent
-            right
-        >
-          <v-table>
-            <tr>
-              <th>ID</th>
-              <th>Thema</th>
-            </tr>
-            <tr v-for="ticket in tickets" :key="ticket.id">
-              <td>{{ ticket.id }}</td>
-              <td>{{ ticket.thema }}</td>
-            </tr>
-          </v-table>
-        </v-navigation-drawer>
+      <v-col cols="3">
+        <v-dialog v-model="dialog" width="auto">
+          <template v-slot:activator="{props}">
+            <v-btn class="mb-4" v-bind="props">Buckets konfigurieren</v-btn>
+          </template>
+          <v-card>
+            <v-card-text>
+              <v-checkbox v-for="bucket in buckets" :key="bucket.id" v-model="selected" :value="bucket.id"
+                          :label=bucket.name @change="sortSelectedBuckets">
+              </v-checkbox>
+            </v-card-text>
+          </v-card>
+        </v-dialog>
+        <v-table>
+          <tr>
+            <th>ID</th>
+            <th>Thema</th>
+          </tr>
+          <tr v-for="paket in pakete" :key="paket.id">
+            <td>{{ paket.id }}</td>
+            <td>{{ paket.thema }}</td>
+          </tr>
+        </v-table>
+        <!--          <v-btn
+                      color="pink"
+                      dark
+                      @click.stop="drawer2 = !drawer2"
+                  >
+                    Toggle
+                  </v-btn>-->
+
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
+import {usePaketeStore} from "@/stores/pakete";
+import {useBucketsStore} from "@/stores/buckets";
+
 export default {
   name: "VergleichView",
   data() {
     return {
-          checked: true,
-          drawer: null,
-          drawer2: null,
-          mini: true,
-          selected: [0, 1, 2, 3, 4],
-          buckets: [{id: 0, name: 'XS'}, {id: 1, name: 'S'}, {id: 2, name: 'M'}, {id: 3, name: 'L'}, {id: 4, name: 'XL'}],
-          tickets: [{id: 0, thema: 'UI erstellen', bucket: 'S'}, {id: 1, thema: 'Suchfunktion', bucket: 'XS'}, {
-        id: 2,
-        thema: 'Migration',
-        bucket: 'M'
-      }]
+      dialog: false,
+      checked: true,
+      drawer: null,
+      drawer2: null,
+      mini: true,
+      selected: [0, 1, 2, 3, 4],
     }
+  },
+  setup() {
+    const paketeStore = usePaketeStore()
+    const bucketStore = useBucketsStore()
+    const buckets = bucketStore.getBuckets
+    const pakete = paketeStore.getFlatView
+    return {buckets, pakete}
   },
   methods: {
     sortSelectedBuckets() {
@@ -125,7 +114,7 @@ export default {
         return a - b
       })
     }
-  }
+  },
 }
 </script>
 
