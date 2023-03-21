c795c8a48deee864e7b59e697ecc52893b4f81a4
Daniel Ackermann <d.ackermann@smf.de>
2023-03-21 12:46:35 +0100

    FlatTree eingebaut, Anzeige ohne Einrückung und +/- Knöpfen

--- src/components/TreeDataCellRenderer.vue
+++ src/components/TreeDataCellRenderer.vue
@@ -1,11 +1,14 @@
 <template>
-  <div :style="'margin-left:' + params.node.data.lvl * 30 + 'px'">
+  <div v-if="tree" :style="'margin-left:' + params.node.data.lvl * 30 + 'px'">
     <v-btn size="x-small" :style="params.node.data.children.length>0 ? 'visibility:visible':'visibility:hidden'"
            @click="changeOpenState">
       <v-icon size="x-small" v-if="!params.node.data.open">mdi-plus</v-icon>
       <v-icon size="x-small" v-else>mdi-minus</v-icon>
     </v-btn>
     {{ params.value }}
+  </div>
+  <div v-else>
+    {{params.value}}
   </div>
 </template>
 
@@ -16,8 +19,15 @@ import {usePaketeStore} from '@/stores/pakete'
 
 export default {
   name: "TestComp",
+  data() {
+    return {
+      tree: true
+    }
+  },
   setup(props) {
-    return {params: props.params};
+    return {
+      params: props.params,
+    };
   },
   methods: {
     changeOpenState() {
@@ -28,11 +38,11 @@ export default {
       this.params.node.setData(aktuellesPaket);
       paketeStore.updateTreeViewAfterChangedOpenState(aktuellesPaket);
       nextTick(() => this.params.api.setRowData(paketeStore.getTreeView))
+    },
+    switchFlatAndTree() {
+      this.tree = !this.tree
+      this.params.api.refreshCells({columns:['ticket_nr']})
     }
   }
 }
 </script>
-
-<style scoped>
-
-</style>
