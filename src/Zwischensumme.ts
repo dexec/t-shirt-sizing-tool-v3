cfa2e0e4bbcf50576473fd40cb1a01512d31d5fe
Daniel <daniel.a.m@web.de>
2023-04-09 15:35:57 +0200

    Zwischensumme aktualisiert

--- src/Zwischensumme.ts
+++ src/Zwischensumme.ts
@@ -1,9 +1,13 @@
-export class Zwischensumme {
+import { AbstrakterAufschlag } from "@/AbstrakterAufschlag";
+
+export class Zwischensumme extends AbstrakterAufschlag{
     vorigerAbschnittAufschlag: number
     vorigerAbschnittAufwand: number
     zwischensummeAufwand: number
 
-    constructor(vorigerAbschnittAufschlag: number, vorigerAbschnittAufwand: number, zwischensummeAufwand: number) {
+
+    constructor(bezeichnung: string, anteilZwischensumme: number, anteilGesamtprojekt: number, vorigerAbschnittAufschlag: number, vorigerAbschnittAufwand: number, zwischensummeAufwand: number) {
+        super(bezeichnung, anteilZwischensumme, anteilGesamtprojekt);
         this.vorigerAbschnittAufschlag = vorigerAbschnittAufschlag;
         this.vorigerAbschnittAufwand = vorigerAbschnittAufwand;
         this.zwischensummeAufwand = zwischensummeAufwand;
