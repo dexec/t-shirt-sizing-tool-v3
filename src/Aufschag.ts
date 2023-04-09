4c55a54942d389652f7b4b7106059aaadbbd06d0
Daniel <daniel.a.m@web.de>
2023-04-09 15:35:09 +0200

    Aufschlag aktualisiert

--- src/Aufschag.ts
+++ src/Aufschag.ts
@@ -1,8 +1,15 @@
-export class Aufschlag {
+import { AbstrakterAufschlag } from "@/AbstrakterAufschlag";
+
+export class Aufschlag extends AbstrakterAufschlag {
     bezeichnung: string
     aufschlag: number
-    constructor(bezeichnung: string, aufschlag: number) {
+    aufwand: number
+
+
+    constructor(bezeichnung: string, anteilZwischensumme: number, anteilGesamtprojekt: number, aufschlag: number, aufwand: number) {
+        super(bezeichnung, anteilZwischensumme, anteilGesamtprojekt);
         this.bezeichnung = bezeichnung;
         this.aufschlag = aufschlag;
+        this.aufwand = aufwand;
     }
 }
