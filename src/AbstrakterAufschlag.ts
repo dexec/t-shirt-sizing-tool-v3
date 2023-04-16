3895d2e23c89ec2c5d423df9274a8a9f36734692
Daniel <daniel.a.m@web.de>
2023-04-16 19:39:09 +0200

    AbstrakerAufschlag erstellt

--- src/AbstrakterAufschlag.ts
+++ src/AbstrakterAufschlag.ts
@@ -1,12 +1,36 @@
 export abstract class AbstrakterAufschlag {
-  bezeichnung: string
-  anteilZwischensumme:number
-  anteilGesamtprojekt:number
+  private _bezeichnung: string;
+  private _anteilZwischensumme: number;
+  private _anteilGesamtprojekt: number;
 
 
   protected constructor(bezeichnung: string, anteilZwischensumme: number, anteilGesamtprojekt: number) {
-    this.bezeichnung = bezeichnung;
-    this.anteilZwischensumme = anteilZwischensumme;
-    this.anteilGesamtprojekt = anteilGesamtprojekt;
+    this._bezeichnung = bezeichnung;
+    this._anteilZwischensumme = anteilZwischensumme;
+    this._anteilGesamtprojekt = anteilGesamtprojekt;
+  }
+
+  get bezeichnung(): string {
+    return this._bezeichnung;
+  }
+
+  set bezeichnung(value: string) {
+    this._bezeichnung = value;
+  }
+
+  get anteilZwischensumme(): number {
+    return this._anteilZwischensumme;
+  }
+
+  set anteilZwischensumme(value: number) {
+    this._anteilZwischensumme = value;
+  }
+
+  get anteilGesamtprojekt(): number {
+    return this._anteilGesamtprojekt;
+  }
+
+  set anteilGesamtprojekt(value: number) {
+    this._anteilGesamtprojekt = value;
   }
 }
