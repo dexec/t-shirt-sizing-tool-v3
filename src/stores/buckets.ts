01bd2164da2b8bf9d601ed6cbb4721fd592fc69e
Daniel Ackermann <d.ackermann@smf.de>
2023-04-19 17:09:19 +0200

    updateAllBuckets entfernt, dedizierte Methoden zum Editieren der Buckets hinzugefügt

--- src/stores/buckets.ts
+++ src/stores/buckets.ts
@@ -9,15 +9,25 @@ export const useBucketsStore = defineStore('buckets', () => {
     for (const bucketFile of saveFile.buckets) {
         buckets.value.push(new Bucket(bucketFile.name))
     }
-    function getBuckets() {
-        return buckets.value;
+
+    function updateBucketName(id: number, newName: string) {
+        const bucketToUpdate = buckets.value.find(bucket => bucket.id == id) as Bucket
+        bucketToUpdate.name = newName;
     }
-    function updateAllBuckets(newBuckets: Bucket[]) {
-        //TODO EINFÜGEN VON NEUEN BUCKETS UND LÖSCHEN VON ALTEN BUCKETS
-        for(const bucket of newBuckets) {
-            const oldBucket = buckets.value.find(currentBucket => currentBucket.id==bucket.id) as Bucket
-            if(oldBucket) oldBucket.name=bucket.name
-        }
+
+    function addNewBucket(idOfSelectedBucket: number, before: boolean): Bucket {
+        const newBucket = new Bucket('');
+        const selectedBucket = buckets.value.find(bucket => bucket.id == idOfSelectedBucket) as Bucket
+        const indexOfNewBucket = before ? buckets.value.indexOf(selectedBucket) : buckets.value.indexOf(selectedBucket) + 1;
+        buckets.value.splice(indexOfNewBucket, 0, newBucket)
+        return newBucket;
     }
-    return {getBuckets, updateAllBuckets}
+
+    function deleteBucket(id: number) {
+        const bucketToDelete = buckets.value.find(bucket => bucket.id == id) as Bucket
+        const indexOfBucketToDelete = buckets.value.indexOf(bucketToDelete)
+        buckets.value.splice(indexOfBucketToDelete, 1);
+    }
+
+    return {buckets, updateBucketName, deleteBucket, addNewBucket}
 });
