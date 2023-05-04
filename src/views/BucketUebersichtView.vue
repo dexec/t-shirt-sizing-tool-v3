0c839600df66f714d999be13d55eee9790bd554b
Daniel Ackermann <d.ackermann@smf.de>
2023-05-04 08:43:56 +0200

    Summe min max hinzugefügt

--- src/views/BucketUebersichtView.vue
+++ src/views/BucketUebersichtView.vue
@@ -61,8 +61,8 @@
           <td>{{ statistiken.summeAlleBucketsGeschaetzt() }}</td>
           <td>{{ statistiken.summeAlleBucketsUngeschaetzt() }}</td>
           <td>{{ statistiken.summeAlleBucketsGesamt() }}</td>
-          <td></td>
-          <td></td>
+          <td>{{statistiken.summeAlleBucketsMin()}}</td>
+          <td>{{statistiken.summeAlleBucketsMax()}}</td>
           <td></td>
           <td></td>
           <td></td>
