59835870a27db8095f8c1be01ab2c7674a0c2591
Daniel Ackermann <d.ackermann@smf.de>
2023-05-09 11:57:59 +0200

    Funktionen sind nun im Kontext Menü verfügbar, erste Keyboard Shortcuts implementiert

--- src/views/PaketUebersichtView.vue
+++ src/views/PaketUebersichtView.vue
@@ -4,38 +4,31 @@
         :columnDefs='columnDefs'
         :defaultColDef='defaultColDef'
         :getRowId='getRowId'
-        :navigateToNextCell='navigateToNextCell'
         :rowData='rowData'
         class='ag-theme-alpine'
         rowSelection='single'
-        style='width: 100%;height: 90%'
+        style='width: 100%;height: 100%'
         suppressRowHoverHighlight='true'
         @cellValueChanged="onCellValueChanged"
+        @contextmenu="rightClickOnCell"
+        @cell-key-down="onCellKeyDown"
         @grid-ready='onGridReady'>
     </ag-grid-vue>
-    <div class='d-flex flex-wrap justify-center fixed'>
-      <v-btn class='mx-5' @click='addNewPaket'>Neues Arbeitspaket anlegen</v-btn>
-      <v-btn class='mx-5' @click='addNewKindPaket'>Neues Arbeitspaket als Kind anlegen</v-btn>
-      <v-btn class='mx-5' @click='removeItem'>Arbeitspaket löschen</v-btn>
-      <v-btn class='mx-5' @click="log">Bucket zuweisen</v-btn>
-      <v-btn class='mx-5' @click='movePaketLeftUp'>
-        <v-icon>mdi-arrow-top-left</v-icon>
-      </v-btn>
-      <v-btn class='mx-5' @click='movePaketLeftDown'>
-        <v-icon>mdi-arrow-bottom-left</v-icon>
-      </v-btn>
-      <v-btn class='mx-5' @click='movePaketUp'>
-        <v-icon>mdi-arrow-up</v-icon>
-      </v-btn>
-      <v-btn class='mx-5' @click='movePaketDown'>
-        <v-icon>mdi-arrow-down</v-icon>
-      </v-btn>
-      <v-btn class='mx-5' @click='movePaketUpRight'>
-        <v-icon>mdi-arrow-top-right</v-icon>
-      </v-btn>
-      <v-btn class='mx-5' @click='movePaketDownRight'>
-        <v-icon>mdi-arrow-bottom-right</v-icon>
-      </v-btn>
+  </div>
+  <div class="wrapper">
+    <div class="content">
+      <div class="menu">
+        <span class="item" @click="addNewPaket">Neues Arbeitspaket anlegen</span>
+        <span class="item" @click="addNewKindPaket">Neues Arbeitspaket als Kind anlegen</span>
+        <span class="item" @click="removeItem">Paket löschen</span>
+        <span class="item" @click="comparePaket">Bucket zuweisen</span>
+        <span class="item" @click="movePaketUp"><v-icon size="x-small">mdi-arrow-up</v-icon></span>
+        <span class="item" @click="movePaketDown"><v-icon size="x-small">mdi-arrow-down</v-icon></span>
+        <span class="item" @click="movePaketLeftUp"><v-icon size="x-small">mdi-arrow-top-left</v-icon></span>
+        <span class="item" @click="movePaketLeftDown"><v-icon size="x-small">mdi-arrow-bottom-left</v-icon></span>
+        <span class="item" @click="movePaketUpRight"><v-icon size="x-small">mdi-arrow-top-right</v-icon></span>
+        <span class="item" @click="movePaketDownRight"><v-icon size="x-small">mdi-arrow-bottom-right</v-icon></span>
+      </div>
     </div>
   </div>
 </template>
@@ -119,7 +112,11 @@ export default {
       gridApi: null,
       columnApi: null,
       defaultColDef: {
-        editable: true
+        editable: true,
+        suppressKeyboardEvent: params => {
+          let key = params.event.key;
+          return params.event.ctrlKey && (key === 'ArrowDown' || key === 'ArrowUp')
+        }
       },
       columnDefs: [
         {
@@ -149,17 +146,18 @@ export default {
           field: 'bucket',
           headerName: 'Bucket',
           valueSetter: (params) => {
-            if(params.newValue==="Zuweisung aufheben") params.data.bucket=null;
+            if (params.newValue === "Zuweisung aufheben") params.data.bucket = null;
             else params.data.bucket = useBucketsStore().buckets.find(bucket => bucket.name === params.newValue);
           },
           valueGetter: (params) => {
-            if(params.data.bucket) return params.data.bucket.name
+            if (params.data.bucket) return params.data.bucket.name
             else return ""
           },
           cellEditor: 'agSelectCellEditor',
           cellEditorParams: {
             values: ["Zuweisung aufheben", ...useBucketsStore().getBucketNames()]
-          }
+          },
+          editable: params => params.data.children.length === 0
         },
         {
           field: 'schaetzung',
@@ -195,8 +193,22 @@ export default {
           params.api.sizeColumnsToFit();
           params.columnApi.autoSizeColumns()
         },*/
-    log() {
-      console.log(this.rowData)
+    showMenu(e) {
+      e.preventDefault();
+      const contextMenu = document.querySelector(".wrapper")
+      let x = e.clientX
+      let y = e.clientY
+      contextMenu.style.left = `${x}px`;
+      contextMenu.style.top = `${y}px`;
+      contextMenu.style.display = "block";
+      //TODO Beim Klick soll der Fokus zurück auf Tabelle gehen
+      document.addEventListener("click", () => contextMenu.style.display = "none");
+    },
+    rightClickOnCell(e) {
+      this.showMenu(e)
+      const focusedRowIndex = this.gridApi.getFocusedCell().rowIndex;
+      const selectedPaket = this.rowData[focusedRowIndex]
+      this.gridApi.getRowNode(selectedPaket.id).setSelected(true)
     },
     onGridReady(params) {
       this.gridApi = params.api;
@@ -209,7 +221,6 @@ export default {
     },
     addNewPaket() {
       let newPaketID = 0;
-      console.log(this.gridApi.getSelectedRows()[0])
       if (this.gridApi.getSelectedRows()[0]) {
         newPaketID = this.paketeStore.addNew(this.gridApi.getSelectedRows()[0].id)
       } else {
@@ -218,7 +229,9 @@ export default {
       this.refreshTable();
       nextTick(() => {
         this.gridApi.getRowNode(newPaketID).setSelected(true);
+        this.gridApi.setFocusedCell(newPaketID)
       });
+      console.log(this.gridApi.getFocusedCell())
     },
     addNewKindPaket() {
       if (this.gridApi.getSelectedRows()[0]) {
@@ -237,7 +250,8 @@ export default {
     },
     movePaketUp() {
       if (this.gridApi.getSelectedRows()[0]) {
-        this.paketeStore.moveUp(this.gridApi.getSelectedRows()[0].id);
+        let paketID = this.gridApi.getSelectedRows()[0].id
+        this.paketeStore.moveUp(paketID);
         this.refreshTable()
       }
     },
@@ -280,31 +294,69 @@ export default {
     },*/
     refreshTable() {
       nextTick(() => {
+        this.gridApi.setFocusedCell(this.gridApi.getSelectedRows()[0].id);
         this.gridApi.refreshCells({force: true});
         this.gridApi.setRowData(this.paketeStore.paketeAsTreeView)
       });
     },
-    navigateToNextCell(params) {
-      const suggestedNextCell = params.nextCellPosition;
-      const KEY_UP = 'ArrowUp';
-      const KEY_DOWN = 'ArrowDown';
-      const noUpOrDownKeyPressed =
-          params.key !== KEY_DOWN && params.key !== KEY_UP;
-      if (noUpOrDownKeyPressed || !suggestedNextCell) {
-        return suggestedNextCell;
-      }
-      params.api.forEachNode(function (node) {
-        if (node.rowIndex === suggestedNextCell.rowIndex) {
-          node.setSelected(true);
+    onCellKeyDown(e) {
+      if (e.event) {
+        let key = e.event.key
+        if (e.event.ctrlKey) {
+          if (key === 'ArrowDown') {
+            this.movePaketDown()
+          }
+          if (key === 'ArrowUp') {
+            this.movePaketUp()
+          }
         }
-      });
-      return suggestedNextCell;
-    }
+        else if(e.event.key==='ArrowUp' || e.event.key==='ArrowDown') {
+          const focusedRowIndex = this.gridApi.getFocusedCell().rowIndex;
+          const selectedPaket = this.rowData[focusedRowIndex]
+          if(selectedPaket) {
+            this.gridApi.getRowNode(selectedPaket.id).setSelected(true)
+          }
+        }
+      }
+    },
   }
 };
 
 </script>
 
-<style>
+<style scoped>
+.wrapper {
+  display: none;
+  position: absolute;
+  width: 280px;
+  border-radius: 10px;
+  background: #fff;
+  box-shadow: 0 12px 35px rgba(0, 0, 0, 0.1);
+}
 
+.wrapper .menu {
+  padding: 10px 12px;
+}
+
+.content .item {
+  list-style: none;
+  height: 20px;
+  display: flex;
+  width: 100%;
+  cursor: pointer;
+  align-items: center;
+  border-radius: 5px;
+  margin-bottom: 2px;
+  padding: 0 5px 0 10px;
+  font-size: 0.8rem;
+}
+
+.content .item:hover {
+  background: #f2f2f2;
+}
+
+.content .item span {
+  margin-left: 8px;
+  font-size: 19px;
+}
 </style>
