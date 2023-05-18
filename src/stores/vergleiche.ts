979cf10ef2feb5c3088f9dcf2baa45d219f48d7e
Daniel Ackermann <d.ackermann@smf.de>
2023-05-18 08:44:02 +0200

    Das Setzen von currentSelectedPaket passiert jetzt im Paketestore

--- src/stores/vergleiche.ts
+++ src/stores/vergleiche.ts
@@ -1,12 +1,12 @@
 import {defineStore} from "pinia";
 import {ref} from "vue";
 import type {Paket} from "@/Paket";
-import {usePaketeStore} from "@/stores/pakete";
 
-export const useVergleicheStore = defineStore('vergleiche',() => {
-    const pakete = usePaketeStore();
+
+export const useVergleicheStore = defineStore('vergleiche', () => {
+
     const currentSelectedPaket = ref<Paket>();
-    if(!currentSelectedPaket.value) currentSelectedPaket.value = pakete.paketeChildrenWithNoBucket()[0];
+
     return {
         currentSelectedPaket,
     }
