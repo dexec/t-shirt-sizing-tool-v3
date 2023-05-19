9c9abf05479f03271c69761c2e6942385ee08fc4
Daniel Ackermann <d.ackermann@smf.de>
2023-05-19 23:02:11 +0200

    Die ausgewählten Buckets werden jetzt auch gespeichert

--- src/stores/vergleiche.ts
+++ src/stores/vergleiche.ts
@@ -6,8 +6,10 @@ import type {Paket} from "@/Paket";
 export const useVergleicheStore = defineStore('vergleiche', () => {
 
     const currentSelectedPaket = ref<Paket>();
+    const currentSelectedBuckets = ref<number[]>([]);
 
     return {
         currentSelectedPaket,
+        currentSelectedBuckets
     }
 })
