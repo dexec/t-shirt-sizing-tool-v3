82415e43131dbd2ac337ac648562ef68b371f718
Daniel Ackermann <d.ackermann@smf.de>
2023-06-06 12:28:29 +0200

    Testing

--- src/views/testView2.vue
+++ src/views/testView2.vue
@@ -1,61 +1,22 @@
-<template>
-  <div class="row">
-    <div class="col-3">
-      <h3>Draggable 1</h3>
-      <draggable
-          class="list-group"
-          :list="list1"
-          group="people"
-          @change="log"
-          itemKey="name"
-      >
-        <template #item="{ element, index }">
-          <div class="list-group-item">{{ element.name }} {{ index }}</div>
-        </template>
-      </draggable>
-    </div>
 
-    <div class="col-3">
-      <h3>Draggable 2</h3>
-      <draggable
-          class="list-group"
-          :list="list2"
-          group="people"
-          @change="log"
-          itemKey="name"
-      >
-        <template #item="{ element, index }">
-          <div class="list-group-item">{{ element.name }} {{ index }}</div>
-        </template>
-      </draggable>
-    </div>
-  </div>
+<template>
+{{count}}
+  <v-btn @click="counter"></v-btn>
 </template>
-<script setup lang="ts">
-import draggable from "vuedraggable";
+
+<script lang="ts" setup>
 import {ref} from "vue";
 
-const list1 = ref( [
-  { name: "John", id: 1 },
-  { name: "Joao", id: 2 },
-  { name: "Jean", id: 3 },
-  { name: "Gerard", id: 4 }
-])
-
-const list2 = ref([
-  { name: "Juan", id: 5 },
-  { name: "Edgard", id: 6 },
-  { name: "Johnson", id: 7 }
-])
-function log(evt:Event) {
-  console.log(evt)
+const count = ref(0);
+function counter() {
+  count.value++;
 }
 </script>
 
 <style scoped>
-
-.ghost {
-  opacity: 0.5;
-  background: #c8ebfb;
+.container {
+  height: calc(100vh - 64px); /* Set the container height to match the viewport */
+  overflow: scroll; /* Enable scrolling when content overflows */
 }
+
 </style>
