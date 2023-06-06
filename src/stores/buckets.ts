cdcba10e77811789e61e2ccf7a7a3bf7d18ecb2d
Daniel Ackermann <d.ackermann@smf.de>
2023-06-06 12:27:24 +0200

    Refactor und types patch

--- src/stores/buckets.ts
+++ src/stores/buckets.ts
@@ -1,11 +1,11 @@
 import {defineStore} from 'pinia';
 import saveFile from './file.json';
 import {Bucket} from "@/Bucket";
-import {ref} from "vue";
 import {usePaketeStore} from "@/stores/pakete";
+import {ref} from "vue";
 
 export const useBucketsStore = defineStore('buckets', () => {
-    const buckets = ref<Array<Bucket>>([]);
+    const buckets = ref<Bucket[]>([])
     for (const bucketFile of saveFile.buckets) {
         const newBucket = new Bucket(bucketFile.name)
         buckets.value.push(newBucket)
@@ -19,9 +19,9 @@ export const useBucketsStore = defineStore('buckets', () => {
         bucketToUpdate.name = newName;
     }
 
-    function addNewBucket(idOfSelectedBucket: number, before: boolean): Bucket {
+    function addNewBucket(id: number, before: boolean): Bucket {
         const newBucket = new Bucket('');
-        const selectedBucket = buckets.value.find(bucket => bucket.id == idOfSelectedBucket) as Bucket
+        const selectedBucket = buckets.value.find(bucket => bucket.id == id) as Bucket
         const indexOfNewBucket = before ? buckets.value.indexOf(selectedBucket) : buckets.value.indexOf(selectedBucket) + 1;
         buckets.value.splice(indexOfNewBucket, 0, newBucket)
         return newBucket;
@@ -29,8 +29,8 @@ export const useBucketsStore = defineStore('buckets', () => {
 
     function deleteBucket(id: number) {
         const pakete = usePaketeStore();
-        const bucketToDelete = buckets.value.find(bucket => bucket.id == id) as Bucket
-        const indexOfBucketToDelete = buckets.value.indexOf(bucketToDelete)
+        const indexOfBucketToDelete = buckets.value.findIndex(bucket => bucket.id==id)
+        const bucketToDelete = buckets.value[indexOfBucketToDelete] as Bucket
         buckets.value.splice(indexOfBucketToDelete, 1);
         pakete.paketeOfBucket(bucketToDelete).forEach(paket => paket.bucket=null)
     }
