602867a20122b4f3ba9893dd225599643a1a891b
Daniel Ackermann <d.ackermann@smf.de>
2023-06-29 08:49:45 +0200

    Vergleichsseite Link entfernt wenn bucketloser modus

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -3,7 +3,7 @@
     <v-tabs>
       <v-tab to="/projekt">Projektübersicht</v-tab>
       <v-tab to="/pakete">Paketübersicht</v-tab>
-      <v-tab to="/vergleich">Vergleich</v-tab>
+      <v-tab v-if="projektStore.bucketmodus" to="/vergleich">Vergleich</v-tab>
       <v-tab to="/buckets">Bucketübersicht</v-tab>
       <v-tab to="/kalkulation">Projektkalkulation</v-tab>
       <v-tab to="/test2">Test2</v-tab>
@@ -12,5 +12,6 @@
 </template>
 
 <script setup>
-  //
+  import {useProjektStore} from "@/stores/projekt";
+  const projektStore = useProjektStore();
 </script>
