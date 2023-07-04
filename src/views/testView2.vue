15ece4e8520a3fc3a5653bddc4ddf6165bfe53b7
Daniel Ackermann <d.ackermann@smf.de>
2023-07-04 11:50:20 +0200

    Test

--- src/views/testView2.vue
+++ src/views/testView2.vue
@@ -1,34 +1,82 @@
 <template>
+  <div class="d-flex">
     <draggable
-        style="overflow-y:scroll;height: 75vh"
-        :list="list"
-        class="dragArea list-group ma-2"
-        ghost-class="destination-item"
+        :list="getPakete()"
+        class="dragArea list-group"
         group="pakete"
         itemKey="name"
-        :sort="false"
+        style="height: 90vh"
     >
-      <template #header>
-        <div class="paket ma-2" style="position: fixed">Überschrift</div>
-      </template>
       <template #item="{ element }">
-          <div class="paket ma-2">
-            <span  class="list-group-item  paketContent">{{
-                element
-              }}
-            </span>
-          </div>
+        <div class="paket ma-2">
+        <span class="list-group-item paketContent">#{{
+            (element as Paket).ticket_nr
+          }}</span>
+          <span class="list-group-item  paketContent">{{
+              (element as Paket).thema
+            }}</span>
+        </div>
       </template>
     </draggable>
+    <draggable
+        :list="getPakete()"
+        class="dragArea list-group"
+        group="pakete"
+        itemKey="name"
+        ghostClass="ghostClass"
+        style="height: 90vh"
+    >
+      <template #item="{ element }">
+        <div class="paket ma-2">
+        <span class="list-group-item paketContent">#{{
+            (element as Paket).ticket_nr
+          }}</span>
+          <span class="list-group-item  paketContent">{{
+              (element as Paket).thema
+            }}</span>
+        </div>
+      </template>
+    </draggable>
+  </div>
 </template>
 
 <script lang="ts" setup>
+import {Paket} from "@/Paket";
+import {usePaketeStore} from "@/stores/pakete";
+import {useBucketsStore} from "@/stores/buckets";
+import {computed, ref} from "vue";
 import draggable from "vuedraggable";
-import {ref} from "vue";
-const list=ref<number[]>([])
-for(let i =0; i<1000;i++) list.value.push(i)
+
+const paketeStore = usePaketeStore();
+const bucketStore = useBucketsStore();
+const dialog = ref(false);
+const checked = ref(false);
+const showPaketeWithoutBucket = ref(true);
+const paketeWithoutBucket = computed(() => paketeStore.paketeChildrenWithNoBucket());
+const paketeChildren = computed(() => paketeStore.paketeChildren());
+const unsortedPaketeListsSortedByBucketsMap = paketeStore.unsortedPaketeListsSortedByBucketsMap;
+const searchedPaket = ref(0);
+
+function getPakete() {
+  return paketeStore.paketeAsFlatView();
+}
 </script>
+
 <style scoped>
+.bucketsButton {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 150px; /* Adjust the width as needed */
+  height: 100px; /* Adjust the height as needed */
+}
+
+.bucketsButtonText {
+  text-align: center;
+  white-space: pre-wrap;
+}
+
 .paket {
   display: flex;
   flex-direction: column;
@@ -47,5 +95,13 @@ for(let i =0; i<1000;i++) list.value.push(i)
 .list-group {
   width: 200px;
   min-height: 250px;
+}
+
+.destination-item {
+  display: none;
+}
+
+.ghostClass {
+  display: none;
 }
 </style>
