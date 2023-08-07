18d5382a265a89bbbb3ceceb63a99ef0e97c839f
Daniel Ackermann <d.ackermann@smf.de>
2023-08-07 17:24:48 +0200

    Testing

--- src/views/testView2.vue
+++ src/views/testView2.vue
@@ -1,74 +1,20 @@
 <template>
-  <v-container>
-  <v-row id="abc">
-    <input type="file" @change="handleFileUpload">
-  </v-row>
-  </v-container>
+<div @contextmenu="rightClick">Right Click</div>
+  <context-menu ref="contextMenuRef" :providedFunctionsProp="[]"></context-menu>
+  <input ref="input">
 </template>
 
-<script lang="ts">
-import {ImportProject} from "@/components/ImportProject";
-export default {
-  methods: {
-    handleFileUpload(event) {
-      const file = event.target.files[0];
-      const reader = new FileReader();
+<script setup>
+import ContextMenu from "@/components/ContextMenu.vue";
+import {onMounted, ref} from "vue";
+const input = ref(null)
+function rightClick(e) {
+  const contextMenuRef = ref(null)
 
-      reader.onload = () => {
-        const fileContents = reader.result;
-        const importProject = ImportProject.getInstance()
-        importProject.initialize(fileContents as string)
-      };
-      reader.readAsText(file);
-    }
-  }
-};
+  console.log(contextMenuRef.value)
+  console.log(input.value)
+
+
+}
 </script>
-<script setup lang="ts">
-import {onMounted} from "vue";
-import AppBar from "@/components/AppBar.vue";
 
-</script>
-<style scoped>
-#drop-area {
-  border: 2px dashed #ccc;
-  border-radius: 20px;
-  width: 480px;
-  font-family: sans-serif;
-  margin: 100px auto;
-  padding: 20px;
-}
-#drop-area.highlight {
-  border-color: purple;
-}
-p {
-  margin-top: 0;
-}
-.my-form {
-  margin-bottom: 10px;
-}
-#gallery {
-  margin-top: 10px;
-}
-#gallery img {
-  width: 150px;
-  margin-bottom: 10px;
-  margin-right: 10px;
-  vertical-align: middle;
-}
-.button {
-  display: inline-block;
-  padding: 10px;
-  background: #ccc;
-  cursor: pointer;
-  border-radius: 5px;
-  border: 1px solid #ccc;
-}
-.button:hover {
-  background: #ddd;
-}
-#fileElem {
-  display: none;
-}
-
-</style>
