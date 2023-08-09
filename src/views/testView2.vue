f50e80288e6862cf83c62ca9914541f77ed6b996
Daniel Ackermann <d.ackermann@smf.de>
2023-08-09 17:27:07 +0200

    Testing

--- src/views/testView2.vue
+++ src/views/testView2.vue
@@ -1,20 +1,23 @@
 <template>
-<div @contextmenu="rightClick">Right Click</div>
-  <context-menu ref="contextMenuRef" :providedFunctionsProp="[]"></context-menu>
-  <input ref="input">
+   <v-btn @click="myCode">TEST</v-btn>
+  <div id="yyy">test</div>
+  <div v-html="mv" />
 </template>
 
 <script setup>
-import ContextMenu from "@/components/ContextMenu.vue";
-import {onMounted, ref} from "vue";
-const input = ref(null)
-function rightClick(e) {
-  const contextMenuRef = ref(null)
+import { ref } from "vue";
 
-  console.log(contextMenuRef.value)
-  console.log(input.value)
+let mv = ref("meinstring");
 
-
+function myCode()
+{
+  window.document.title = "hallo welt";
+  document.getElementById("yyy").innerHTML = "<b><i>v-html demonstriert, so funktioniert es intern</i></b>";
+  mv.value = "<b><i>v-html demonstriert, so funktioniert es intern</i></b>";
 }
+
+const string1and2 = ref("");
+string1and2.value="string 1" + "\r\nstring 2";
+
 </script>
 
