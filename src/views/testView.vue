a9ffc862a670098ac934617f2110c05bd1efdaa2
Daniel Ackermann <d.ackermann@smf.de>
2023-08-11 14:31:22 +0200

    Test

--- src/views/testView.vue
+++ src/views/testView.vue
@@ -1,11 +1,21 @@
 <template>
-  <h1>TestView</h1>
-  <p>{{firstName}}, {{lastName}}</p>
+  <v-btn @click="myCode">TEST</v-btn>
+  <div id="yyy">test</div>
+  <div v-html="mv" />
 </template>
 
-<script setup lang="ts">
-defineProps({
-  firstName: String,
-  lastName: String
-})
+<script setup>
+import { ref } from "vue";
+let mv = ref("meinstring");
+
+function myCode()
+{
+  window.document.title = "hallo welt";
+  document.getElementById("yyy").innerHTML = "<b><i>v-html demonstriert, so funktioniert es intern</i></b>";
+  mv.value = "<b><i>v-html demonstriert, so funktioniert es intern</i></b>";
+}
+
+const string1and2 = ref("");
+string1and2.value="string 1" + "\r\nstring 2";
+
 </script>
