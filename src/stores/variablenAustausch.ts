efab6169aa1b895e84615d76aad9d5a673662221
Daniel Ackermann <d.ackermann@smf.de>
2023-08-14 18:33:13 +0200

    Einrückung

--- src/stores/variablenAustausch.ts
+++ src/stores/variablenAustausch.ts
@@ -1,11 +1,11 @@
-import {defineStore} from "pinia";
-import {ref} from "vue";
+import { defineStore } from "pinia";
+import { ref } from "vue";
 
-export const useVariablenAustauschStore = defineStore("variablenAustausch",()=>{
-    const geladen = ref<boolean>(false);
-    const searchPaketString = ref<string>("")
-    return {
-        geladen,
-        searchPaketString,
-    }
-})
+export const useVariablenAustauschStore = defineStore("variablenAustausch", () => {
+  const geladen = ref<boolean>(false);
+  const searchPaketString = ref<string>("");
+  return {
+    geladen,
+    searchPaketString
+  };
+});
