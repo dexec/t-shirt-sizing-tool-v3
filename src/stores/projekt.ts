7e20b15139ccc4702b2875d78eb3d96a2d6fe992
Daniel Ackermann <d.ackermann@smf.de>
2023-08-14 18:33:01 +0200

    Einrückung

--- src/stores/projekt.ts
+++ src/stores/projekt.ts
@@ -1,13 +1,13 @@
-import {defineStore} from "pinia";
-import {ref} from "vue";
+import { defineStore } from "pinia";
+import { ref } from "vue";
 
-export const useProjektStore = defineStore('projekt', () => {
-    const projektname = ref<string>("");
-    const projektbeschreibung = ref<string>("");
-    const bucketmodus = ref<boolean>(false);
-    return {
-        projektname,
-        projektbeschreibung,
-        bucketmodus
-    }
-})
+export const useProjektStore = defineStore("projekt", () => {
+  const projektname = ref<string>("");
+  const projektbeschreibung = ref<string>("");
+  const bucketmodus = ref<boolean>(false);
+  return {
+    projektname,
+    projektbeschreibung,
+    bucketmodus
+  };
+});
