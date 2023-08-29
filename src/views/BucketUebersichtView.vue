66c4defd5e812138f1159354db95f9bc484cc87f
Daniel Ackermann <d.ackermann@smf.de>
2023-08-29 15:44:18 +0200

    Alle Angaben werden gerundet

--- src/views/BucketUebersichtView.vue
+++ src/views/BucketUebersichtView.vue
@@ -28,48 +28,37 @@
       <tbody>
       <tr v-for="statistik of statistiken.statistiken" :key="statistik.bucket.id">
         <td>{{ statistik.bucket.name }}</td>
-        <td>{{ statistik.anzahlGeschaetzt }}</td>
-        <td>{{ statistik.anzahlUngeschaetzt }}</td>
-        <td>{{ statistik.anzahlGesamt }}</td>
-        <td>{{ statistik.min }}</td>
-        <td>{{ statistik.max }}</td>
-        <td>{{ statistik.anteilAnzahl * 100 + "%" }}</td>
-        <td>{{ statistik.durchschnitt }}</td>
-        <td>{{ statistik.median }}</td>
-        <td>{{ statistik.summeSchaetzungen }}</td>
-        <td>
-          {{
-            statistiken.summeAlleBucketsSchaetzungenSumme() == null || statistik.anzahlGeschaetzt==0  ? "" : (statistiken.summeAlleBucketsSchaetzungenSumme() == 0 ? "0%" :Math.round(statistik.summeSchaetzungen / statistiken.summeAlleBucketsSchaetzungenSumme() * 100) + "%")
-          }}
+        <td>{{ parseFloat(statistik.anzahlGeschaetzt.toFixed(projektStore.nachkommastellen)) }}</td>
+        <td>{{ parseFloat(statistik.anzahlUngeschaetzt.toFixed(projektStore.nachkommastellen)) }}</td>
+        <td>{{ parseFloat(statistik.anzahlGesamt.toFixed(projektStore.nachkommastellen)) }}</td>
+        <td>{{ minBucketmodus(statistik as Statistik) }}</td>
+        <td>{{ maxBucketmodus(statistik as Statistik) }}</td>
+        <td>{{ parseFloat((statistik.anteilAnzahl * 100).toFixed(projektStore.nachkommastellen)) + "%" }}</td>
+        <td>{{ durchschnittBucketmodus(statistik as Statistik) }}</td>
+        <td>{{ medianBucketmodus(statistik as Statistik) }}</td>
+        <td>{{ schaetzungenSummeBucketmodusPT(statistik as Statistik) }}
         </td>
-        <td>{{ statistik.summeDurchschnitt }}</td>
-        <td>
-          {{
-            statistiken.summeAlleBucketsDurchschnittSumme() == null || statistik.anzahlGeschaetzt == 0 ? "" : (statistiken.summeAlleBucketsDurchschnittSumme()==0?"0%":Math.round(statistik.summeDurchschnitt / statistiken.summeAlleBucketsDurchschnittSumme() * 100) + "%")
-          }}
-        </td>
-        <td>{{ statistik.summeMedian }}</td>
-        <td>
-          {{
-            statistiken.summeAlleBucketsMedianSumme() == null || statistik.anzahlGeschaetzt == 0 ? "" : (statistiken.summeAlleBucketsMedianSumme()==0?"0%":Math.round(statistik.summeMedian / statistiken.summeAlleBucketsMedianSumme() * 100) + "%")
-          }}
-        </td>
+        <td>{{ schaetzungenSummeBucketmodusProzent(statistik as Statistik) }}</td>
+        <td>{{ durchschnittSummeBucketmodusPT(statistik as Statistik) }}</td>
+        <td>{{ durchschnittSummeBucketmodusProzent(statistik as Statistik) }}</td>
+        <td>{{ medianSummeBucketmodusPT(statistik as Statistik) }}</td>
+        <td>{{ medianSummeBucketmodusProzent(statistik as Statistik) }}</td>
       </tr>
       <!--        TODO Keine Summenreihe, wenn bucketmodus aber keine Buckets definiert sind.
               Dann lieber Nachricht, dass man Buckets definieren sollte-->
       <tr class="font-weight-bold">
         <td>Summe</td>
-        <td>{{ statistiken.summeAlleBucketsGeschaetzt() }}</td>
-        <td>{{ statistiken.summeAlleBucketsUngeschaetzt() }}</td>
-        <td>{{ statistiken.summeAlleBucketsGesamt() }}</td>
-        <td>{{ statistiken.summeAlleBucketsMin() }}</td>
-        <td>{{ statistiken.summeAlleBucketsMax() }}</td>
+        <td>{{ parseFloat(statistiken.summeAlleBucketsGeschaetzt().toFixed(projektStore.nachkommastellen)) }}</td>
+        <td>{{ parseFloat(statistiken.summeAlleBucketsUngeschaetzt().toFixed(projektStore.nachkommastellen)) }}</td>
+        <td>{{ parseFloat(statistiken.summeAlleBucketsGesamt().toFixed(projektStore.nachkommastellen)) }}</td>
+        <td>{{ minSumme() }}</td>
+        <td>{{ maxSumme() }}</td>
         <td></td>
         <td></td>
         <td></td>
-        <td colspan="2">{{ statistiken.summeAlleBucketsSchaetzungenSumme() }}</td>
-        <td colspan="2">{{ statistiken.summeAlleBucketsDurchschnittSumme() }}</td>
-        <td colspan="2">{{ statistiken.summeAlleBucketsMedianSumme() }}</td>
+        <td colspan="2">{{ summeSchaetzungenSumme() }}</td>
+        <td colspan="2">{{ summeDurchschnittSumme() }}</td>
+        <td colspan="2">{{ summeMedianSumme() }}</td>
       </tr>
       </tbody>
     </template>
@@ -78,30 +67,30 @@
     <template v-slot:default>
       <thead>
       <tr>
-        <th >Anzahl geschätzt</th>
-        <th >Anzahl ungeschätzt</th>
+        <th>Anzahl geschätzt</th>
+        <th>Anzahl ungeschätzt</th>
         <th>Anzahl gesamt</th>
-        <th >Min</th>
-        <th >Max</th>
-        <th >Mittelwert</th>
-        <th >Median</th>
-        <th >Summe Schätzungen</th>
-        <th >Durchschnittliche Summe</th>
-        <th >Mediane Summe</th>
+        <th>Min</th>
+        <th>Max</th>
+        <th>Mittelwert</th>
+        <th>Median</th>
+        <th>Summe Schätzungen</th>
+        <th>Durchschnittliche Summe</th>
+        <th>Mediane Summe</th>
       </tr>
       </thead>
       <tbody>
       <tr class="font-weight-bold">
-        <td>{{ statistiken.summeAlleBucketsGeschaetzt() }}</td>
-        <td>{{ statistiken.summeAlleBucketsUngeschaetzt() }}</td>
-        <td>{{ statistiken.summeAlleBucketsGesamt() }}</td>
-        <td>{{ statistiken.summeAlleBucketsMin() }}</td>
-        <td>{{ statistiken.summeAlleBucketsMax() }}</td>
-        <td>{{ statistiken.summeAlleBucketsDurchschnitt() }}</td>
-        <td>{{ statistiken.summeAlleBucketsMedian() }}</td>
-        <td>{{ statistiken.summeAlleBucketsSchaetzungenSumme() }}</td>
-        <td>{{ statistiken.summeAlleBucketsDurchschnittSumme() }}</td>
-        <td>{{ statistiken.summeAlleBucketsMedianSumme() }}</td>
+        <td>{{ parseFloat(statistiken.summeAlleBucketsGeschaetzt().toFixed(projektStore.nachkommastellen)) }}</td>
+        <td>{{ parseFloat(statistiken.summeAlleBucketsUngeschaetzt().toFixed(projektStore.nachkommastellen)) }}</td>
+        <td>{{ parseFloat(statistiken.summeAlleBucketsGesamt().toFixed(projektStore.nachkommastellen)) }}</td>
+        <td>{{ minSumme() }}</td>
+        <td>{{ maxSumme() }}</td>
+        <td>{{ durchschnittSumme() }}</td>
+        <td>{{ medianSumme() }}</td>
+        <td>{{ summeSchaetzungenSumme() }}</td>
+        <td>{{ summeDurchschnittSumme() }}</td>
+        <td>{{ summeMedianSumme() }}</td>
       </tr>
       </tbody>
     </template>
@@ -111,10 +100,144 @@
 <script lang="ts" setup>
 import {useStatistikenStore} from "@/stores/statistiken";
 import {useProjektStore} from "@/stores/projekt";
+import type {Statistik} from "@/Statistik";
 
 const statistiken = useStatistikenStore();
 const projektStore = useProjektStore();
 statistiken.berechne();
+function minBucketmodus(statistik: Statistik) {
+  if(statistik.min != null) {
+    return parseFloat(statistik.min.toFixed(projektStore.nachkommastellen));
+  }
+  return ""
+}
+
+function maxBucketmodus(statistik: Statistik) {
+  if(statistik.max != null) {
+    return parseFloat(statistik.max.toFixed(projektStore.nachkommastellen));
+  }
+  return ""
+}
+
+function durchschnittBucketmodus(statistik: Statistik) {
+  if(statistik.durchschnitt != null) {
+    return parseFloat(statistik.durchschnitt.toFixed(projektStore.nachkommastellen))
+  }
+  return ""
+}
+
+function medianBucketmodus(statistik: Statistik) {
+  if(statistik.median != null) {
+    return parseFloat(statistik.median.toFixed(projektStore.nachkommastellen))
+  }
+  return ""
+}
+function schaetzungenSummeBucketmodusPT(statistik: Statistik) {
+  if (statistik.summeSchaetzungen != null) {
+    return parseFloat(statistik.summeSchaetzungen.toFixed(projektStore.nachkommastellen));
+  }
+  return "";
+}
+
+function schaetzungenSummeBucketmodusProzent(statistik: Statistik) {
+  const summeSchaetzunSumme = statistiken.summeAlleBucketsSchaetzungenSumme();
+  if (summeSchaetzunSumme == null || statistik.anzahlGeschaetzt == 0) {
+    return "";
+  } else {
+    if (summeSchaetzunSumme == 0) {
+      return "0%";
+    } else {
+      return parseFloat((statistik.summeSchaetzungen! / summeSchaetzunSumme * 100).toFixed(projektStore.nachkommastellen)) + "%"
+    }
+  }
+
+}
+
+function durchschnittSummeBucketmodusPT(statistik: Statistik) {
+  if (statistik.summeDurchschnitt != null) {
+    return parseFloat(statistik.summeDurchschnitt.toFixed(projektStore.nachkommastellen));
+  }
+  return "";
+}
+
+function durchschnittSummeBucketmodusProzent(statistik: Statistik) {
+  const summeDurchschnittSumme = statistiken.summeAlleBucketsDurchschnittSumme();
+  if (summeDurchschnittSumme == null || statistik.anzahlGeschaetzt == 0) {
+    return "";
+  } else {
+    if (summeDurchschnittSumme == 0) {
+      return "0%";
+    } else {
+      return parseFloat((statistik.summeDurchschnitt! / summeDurchschnittSumme * 100).toFixed(projektStore.nachkommastellen)) + "%";
+    }
+  }
+
+}
+
+function medianSummeBucketmodusPT(statistik: Statistik) {
+  if (statistik.summeMedian != null) {
+    return parseFloat(statistik.summeMedian.toFixed(projektStore.nachkommastellen))
+  }
+  return ""
+}
+
+function medianSummeBucketmodusProzent(statistik: Statistik) {
+  const summeMedianSumme = statistiken.summeAlleBucketsMedianSumme();
+  console.log(summeMedianSumme)
+  if (summeMedianSumme == null || statistik.anzahlGeschaetzt == 0) {
+    return ""
+  } else {
+    if (summeMedianSumme == 0) {
+      return "0%"
+    } else {
+      return parseFloat((statistik.summeMedian! / summeMedianSumme * 100).toFixed(projektStore.nachkommastellen)) + "%"
+    }
+  }
+}
+
+function minSumme() {
+  const summeMin = statistiken.summeAlleBucketsMin();
+  if (summeMin != null) {
+    return parseFloat(summeMin.toFixed(projektStore.nachkommastellen));
+  } else return ""
+}
+
+function maxSumme() {
+  const summeMax = statistiken.summeAlleBucketsMax();
+  if (summeMax != null) return parseFloat(summeMax.toFixed(projektStore.nachkommastellen));
+  else return ""
+}
+
+function durchschnittSumme() {
+  const summeDurchschnitt = statistiken.summeAlleBucketsDurchschnitt();
+  if (summeDurchschnitt != null) return parseFloat(summeDurchschnitt.toFixed(projektStore.nachkommastellen))
+  else return ""
+}
+
+function medianSumme() {
+  const summeMedian = statistiken.summeAlleBucketsMedian();
+  if (summeMedian != null) return parseFloat(summeMedian.toFixed(projektStore.nachkommastellen))
+  else return ""
+}
+
+function summeSchaetzungenSumme() {
+  const summeSchaetzungen = statistiken.summeAlleBucketsSchaetzungenSumme()
+  if (summeSchaetzungen != null) return parseFloat(summeSchaetzungen.toFixed(projektStore.nachkommastellen))
+  else return ""
+}
+
+function summeDurchschnittSumme() {
+  const summeDurchschnitt = statistiken.summeAlleBucketsDurchschnittSumme()
+  if (summeDurchschnitt != null) return parseFloat(summeDurchschnitt.toFixed(projektStore.nachkommastellen));
+  else return ""
+}
+
+function summeMedianSumme() {
+  const summeMedian = statistiken.summeAlleBucketsMedianSumme()
+  if (summeMedian != null) return parseFloat(summeMedian.toFixed(projektStore.nachkommastellen))
+  else return ""
+}
+
 </script>
 
 <style scoped>
