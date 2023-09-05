19b927918d9aff11e0ef5dde65f1de1baedd61a2
Daniel Ackermann <d.ackermann@smf.de>
2023-09-05 15:30:43 +0200

    Suchtextfeld ausgelagert

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -6,16 +6,14 @@
       <v-tab v-if="projektStore.bucketmodus" to="/vergleich">Vergleich</v-tab>
       <v-tab to="/buckets">Bucketübersicht</v-tab>
       <v-tab to="/kalkulation">Projektkalkulation</v-tab>
+      <v-tab to="/test">Test</v-tab>
       <v-tab to="/test2">Test2</v-tab>
     </v-tabs>
-    <v-text-field style="width:200px; position: absolute; right: 10px" v-model="variablenAustauschStore.searchPaketString" clearable @click:clear="variablenAustauschStore.searchPaketString=''" label="Pakete filtern"></v-text-field>
   </v-app-bar>
 </template>
 
 <script setup>
   import {useProjektStore} from "@/stores/projekt";
-  import {useVariablenAustauschStore} from "@/stores/variablenAustausch";
   const projektStore = useProjektStore();
-  const variablenAustauschStore = useVariablenAustauschStore();
 
 </script>
