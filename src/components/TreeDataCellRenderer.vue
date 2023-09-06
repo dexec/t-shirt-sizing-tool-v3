fd9295e53fde4abd51cb5c89c2bba174629fd89a
Daniel Ackermann <d.ackermann@smf.de>
2023-09-06 17:27:23 +0200

    Refactor

--- src/components/TreeDataCellRenderer.vue
+++ src/components/TreeDataCellRenderer.vue
@@ -1,19 +1,19 @@
 <template>
   <div :style="'margin-left:' + params.node.data.lvl * 30 + 'px'">
-    <v-btn size="x-small" :style="params.node.data.children.length>0 ? 'visibility:visible':'visibility:hidden'"
+    <v-btn :style="params.node.data.children.length>0 ? 'visibility:visible':'visibility:hidden'" size="x-small"
            @click="changeOpenState">
-      <v-icon size="x-small" v-if="!params.node.data.open">mdi-plus</v-icon>
-      <v-icon size="x-small" v-else>mdi-minus</v-icon>
+      <v-icon v-if="!params.node.data.open" size="x-small">mdi-plus</v-icon>
+      <v-icon v-else size="x-small">mdi-minus</v-icon>
     </v-btn>
     {{ params.value }}
   </div>
 </template>
-<script setup lang="ts">
+<script lang="ts" setup>
 
-import {nextTick} from "vue";
-import {usePaketeStore} from '@/stores/pakete'
+import { nextTick } from "vue";
+import { usePaketeStore } from "@/stores/pakete";
 
-const props = defineProps(['params']);
+const props = defineProps(["params"]);
 
 function changeOpenState() {
   const paketeStore = usePaketeStore();
@@ -25,39 +25,6 @@ function changeOpenState() {
   nextTick(() => {
     props.params.api.setRowData(paketeStore.paketeAsTreeView);
     props.params.api.setFocusedCell(props.params.api.getRowNode(aktuellesPaket.id).rowIndex, props.params.column);
-  })
-}
-
-function switchFlatAndTree() {
-
+  });
 }
 </script>
-<!--<script>
-
-import {nextTick} from "vue";
-import {usePaketeStore} from '@/stores/pakete'
-
-export default {
-  name: "TestComp",
-  setup(props) {
-    return {
-      params: props.params,
-    };
-  },
-  methods: {
-    changeOpenState() {
-      console.log(this.params)
-      const paketeStore = usePaketeStore();
-      let aktuellesPaket = this.params.node.data;
-      aktuellesPaket.open = !aktuellesPaket.open;
-      this.params.node.setSelected(true)
-      this.params.node.setData(aktuellesPaket);
-      paketeStore.updateTreeViewAfterChangedOpenState(aktuellesPaket);
-      nextTick(() => this.params.api.setRowData(paketeStore.paketeAsTreeView))
-    },
-    switchFlatAndTree() {
-
-    }
-  }
-}
-</script>-->
