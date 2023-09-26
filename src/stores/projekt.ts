2f0c7aecb4b81c5e4ac2fde7ece38abc4abd3bb8
Daniel Ackermann <d.ackermann@smf.de>
2023-09-26 12:30:43 +0200

    Rundungsarten entfernt

--- src/stores/projekt.ts
+++ src/stores/projekt.ts
@@ -1,20 +1,17 @@
 import { defineStore } from "pinia";
 import { ref } from "vue";
-import { RundungsartET } from "@/enums/RundungsartET";
 
 export const useProjektStore = defineStore("projekt", () => {
 
   const projektname = ref<string>("");
   const projektbeschreibung = ref<string>("");
   const bucketmodus = ref<boolean>(false);
-  const rundungsart = ref(RundungsartET.KAUFMAENNISCH);
   const nachkommastellen = ref<number>(2);
 
   return {
     projektname,
     projektbeschreibung,
     bucketmodus,
-    rundungsart,
     nachkommastellen
   };
 });
