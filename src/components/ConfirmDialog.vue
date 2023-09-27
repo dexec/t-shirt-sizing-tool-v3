3f9e6f50dd19f0edb71bbf13bd84b6261db0d4ec
Daniel Ackermann <d.ackermann@smf.de>
2023-09-27 16:38:16 +0200

    Erste Versuche Shortcuts umzusetzen

--- src/components/ConfirmDialog.vue
+++ src/components/ConfirmDialog.vue
@@ -1,32 +1,51 @@
 <template>
-  <div class="text-center">
+  <div id="confirmDialogContainer" class="text-center">
     <v-dialog
-        v-model="dialog"
-        width="auto"
+      v-model="dialog"
+      width="auto"
     >
       <v-card>
         <v-card-text>
-          {{props.text}}
+          {{ props.text }}
         </v-card-text>
         <v-card-actions class="d-flex flex-column">
-          <v-btn color="primary" block @click="dialog = false; confirmFunction()">Bestätigen</v-btn>
-          <v-btn color="primary" block @click="dialog = false; cancelFunction()">Abbrechen</v-btn>
+          <v-btn block color="primary" @click="dialog = false; confirmFunction()" @keydown.enter="log()">Bestätigen
+          </v-btn>
+          <v-btn block color="primary" @click="dialog = false; cancelFunction()">Abbrechen
+          </v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
   </div>
 </template>
-<script setup lang="ts">
+<script lang="ts" setup>
+import { onMounted, ref } from "vue";
+
+const props = defineProps(["text", "confirmFunction", "cancelFunction"]);
+const dialog = ref(false);
+
 //TODO Keyboard shortcuts
-import {ref} from "vue";
-const props = defineProps(['text','confirmFunction','cancelFunction'])
-const dialog = ref(false)
-
-function showDialog() {
-  dialog.value = true
+function log() {
+  console.log('abc')
 }
+function showDialog() {
+  dialog.value = true;
+}
+/*
+onMounted(() => {
+  document.addEventListener("keyup", (e) => {
+    if (dialog.value) {
+      if (e.key == "Enter" || e.key == "Esc") {
+        dialog.value = false;
+        document.getElementById('confirmDialogContainer')?.focus()
+        if (e.key == "Enter") props.confirmFunction();
+        else if (e.key == "Esc") props.cancelFunction();
+      }
+    }
+  });
+});*/
 
-defineExpose({showDialog})
+defineExpose({ showDialog });
 </script>
 <style scoped>
 
