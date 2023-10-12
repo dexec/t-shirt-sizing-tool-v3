cc8281b0b28b58d2af0962751d937479f957666d
Daniel Ackermann <d.ackermann@smf.de>
2023-10-12 08:57:21 +0200

    Testing

---
+++ src/components/testComp.vue
@@ -0,0 +1,24 @@
+<template>
+  <div ref="reftodiv" class="container" @keyup.enter="handle"></div>
+</template>
+
+<script lang="ts" setup>
+import { nextTick, onMounted, ref } from "vue";
+
+const reftodiv = ref<HTMLDivElement | null>(null);
+
+reftodiv.value?.addEventListener("keyup",handle)
+onMounted(() => nextTick(() => reftodiv.value?.focus()));
+function handle() {
+  console.log("enteredSubComp");
+}
+</script>
+
+
+<style scoped>
+.container {
+  width: 400px;
+  height: 400px;
+  border: black solid 2px;
+}
+</style>
--- src/views/testView.vue
+++ src/views/testView.vue
@@ -1,56 +1,12 @@
 <template>
-  <div>
-    <div class="original-div">
-      <!-- Your original content here -->
-      <v-btn @click="log()"></v-btn>
-    </div>
+  <div @keyup.enter="console.log('entered')">
+    <test-comp>
 
-    <button class="clickable-element" @click="createBlurredDiv">Create Blurred Div</button>
-
-    <!-- Render the blurred div when the blurredDivVisible is true -->
-    <div v-if="blurredDivVisible" class="blurred-div d-flex flex-column justify-center">
-      <!-- Content for the blurred div -->
-      <h2>Blurred Div Content</h2>
-      <p>This is some content in the blurred div.</p>
-    </div>
+    </test-comp>
   </div>
 </template>
 
 <script setup>
-import {ref} from 'vue';
 
-
-const blurredDivVisible = ref(false) // Initially, the blurred div is not visible
-
-
-function createBlurredDiv() {
-  // Toggle the visibility of the blurred div
-  blurredDivVisible.value = !blurredDivVisible.value;
-}
-
-function log() {
-  console.log("log")
-}
+import TestComp from "@/components/testComp.vue";
 </script>
-
-<style>
-@import "@/styles/hoveLink.css";
-.original-div {
-  /* Your styles for the original div */
-  width: 300px;
-  height: 200px;
-  background-color: lightblue;
-}
-
-.blurred-div {
-  /* Styles for the blurred div */
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  backdrop-filter: blur(10px); /* Adjust the blur radius as needed */
-  z-index: 1; /* Ensure the blurred div is on top */
-  background-color: rgba(255, 255, 255, 0.5); /* Add a semi-transparent background color for better visibility */
-}
-</style>
