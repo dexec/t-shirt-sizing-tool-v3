6712cc6389c092a1bc776ec1048b2d59463358b1
Daniel Ackermann <d.ackermann@smf.de>
2023-10-12 08:56:16 +0200

    Neues Bucket wird "Neu" genannt

--- src/stores/buckets.ts
+++ src/stores/buckets.ts
@@ -1,75 +1,71 @@
-import {defineStore} from "pinia";
-import {Bucket} from "@/models/Bucket";
-import {usePaketeStore} from "@/stores/pakete";
-import {ref} from "vue";
+import { defineStore } from "pinia";
+import { Bucket } from "@/models/Bucket";
+import { usePaketeStore } from "@/stores/pakete";
+import { ref } from "vue";
 
 export const useBucketsStore = defineStore("buckets", () => {
-    const bucketsAsSortedArray = ref<Bucket[]>([]);
+  const bucketsAsSortedArray = ref<Bucket[]>([]);
 
-    const bucketsAsMap = ref(new Map<number, Bucket>());
+  const bucketsAsMap = ref(new Map<number, Bucket>());
 
-    function getBucketNamesSorted() {
-        return bucketsAsSortedArray.value.map(bucket => bucket.name);
+  function getBucketNamesSorted() {
+    return bucketsAsSortedArray.value.map(bucket => bucket.name);
+  }
+
+  function updateBucketName(id: number, newName: string) {
+    const bucketToUpdate = bucketsAsMap.value.get(id);
+    if (bucketToUpdate) {
+      bucketToUpdate.name = newName;
     }
+  }
 
-    function updateBucketName(id: number, newName: string) {
-        const bucketToUpdate = bucketsAsMap.value.get(id);
-        if (bucketToUpdate) {
-            bucketToUpdate.name = newName;
-        }
+  function addNewBucket(id: number, before: boolean): Bucket {
+    const pakete = usePaketeStore();
+    let newBucketName = "neu";
+    for (let i = 1; ; i++) {
+      if (getBucketNamesSorted().includes(newBucketName)) {
+        newBucketName = "neu" +i
+      } else {
+        break;
+      }
     }
+    const newBucket = new Bucket(newBucketName);
+    bucketsAsMap.value.set(newBucket.id, newBucket);
+    const selectedBucket = bucketsAsSortedArray.value.find(bucket => bucket.id == id) as Bucket;
+    const indexOfNewBucket = before ? bucketsAsSortedArray.value.indexOf(selectedBucket) : bucketsAsSortedArray.value.indexOf(selectedBucket) + 1;
+    bucketsAsSortedArray.value.splice(indexOfNewBucket, 0, newBucket);
+    pakete.unsortedPaketeListsSortedByBucketsMap.set(newBucket, []);
+    return newBucket;
+  }
 
-    function addNewBucket(id: number, before: boolean): Bucket {
-        const pakete = usePaketeStore();
-        let lastAddedBucketName;
-        let newBucketName;
-        if (bucketsAsMap.value.size == 0) newBucketName = 'S'
-        else {
-            lastAddedBucketName = bucketsAsSortedArray.value.reduce((a, b) => a.id > b.id ? a : b).name
-            if (/^\d+$/.test(lastAddedBucketName.slice(-1))) {
-                const lastCharAsInt = parseInt(lastAddedBucketName.slice(-1));
-                newBucketName = lastAddedBucketName.slice(0, -1) + (lastCharAsInt + 1)
-            } else {
-                newBucketName = lastAddedBucketName + "1"
-            }
-        }
-        const newBucket = new Bucket(newBucketName);
-        bucketsAsMap.value.set(newBucket.id, newBucket);
-        const selectedBucket = bucketsAsSortedArray.value.find(bucket => bucket.id == id) as Bucket;
-        const indexOfNewBucket = before ? bucketsAsSortedArray.value.indexOf(selectedBucket) : bucketsAsSortedArray.value.indexOf(selectedBucket) + 1;
-        bucketsAsSortedArray.value.splice(indexOfNewBucket, 0, newBucket);
-        pakete.unsortedPaketeListsSortedByBucketsMap.set(newBucket, []);
-        return newBucket;
+  function deleteBucket(id: number) {
+    const pakete = usePaketeStore();
+    const bucketToDelete = bucketsAsMap.value.get(id);
+    if (bucketToDelete) {
+      const indexOfBucketToDelete = bucketsAsSortedArray.value.findIndex(bucket => bucketToDelete == bucket);
+      bucketsAsSortedArray.value.splice(indexOfBucketToDelete, 1);
+      pakete.paketeOfBucket(bucketToDelete).forEach(paket => paket.bucket = null);
+      pakete.unsortedPaketeListsSortedByBucketsMap.delete(bucketToDelete);
+      bucketsAsMap.value.delete(id);
     }
+  }
 
-    function deleteBucket(id: number) {
-        const pakete = usePaketeStore();
-        const bucketToDelete = bucketsAsMap.value.get(id);
-        if (bucketToDelete) {
-            const indexOfBucketToDelete = bucketsAsSortedArray.value.findIndex(bucket => bucketToDelete == bucket);
-            bucketsAsSortedArray.value.splice(indexOfBucketToDelete, 1);
-            pakete.paketeOfBucket(bucketToDelete).forEach(paket => paket.bucket = null);
-            pakete.unsortedPaketeListsSortedByBucketsMap.delete(bucketToDelete);
-            bucketsAsMap.value.delete(id);
-        }
+  function swapWithBucket(id: number, before: boolean) {
+    const indexOfbucketToSwap = bucketsAsSortedArray.value.findIndex(bucket => bucket.id == id);
+    if (before && indexOfbucketToSwap > 0) {
+      [bucketsAsSortedArray.value[indexOfbucketToSwap - 1], bucketsAsSortedArray.value[indexOfbucketToSwap]] = [bucketsAsSortedArray.value[indexOfbucketToSwap], bucketsAsSortedArray.value[indexOfbucketToSwap - 1]];
+    } else if (!before && indexOfbucketToSwap < bucketsAsSortedArray.value.length - 1) {
+      [bucketsAsSortedArray.value[indexOfbucketToSwap + 1], bucketsAsSortedArray.value[indexOfbucketToSwap]] = [bucketsAsSortedArray.value[indexOfbucketToSwap], bucketsAsSortedArray.value[indexOfbucketToSwap + 1]];
     }
+  }
 
-    function swapWithBucket(id: number, before: boolean) {
-        const indexOfbucketToSwap = bucketsAsSortedArray.value.findIndex(bucket => bucket.id == id);
-        if (before && indexOfbucketToSwap > 0) {
-            [bucketsAsSortedArray.value[indexOfbucketToSwap - 1], bucketsAsSortedArray.value[indexOfbucketToSwap]] = [bucketsAsSortedArray.value[indexOfbucketToSwap], bucketsAsSortedArray.value[indexOfbucketToSwap - 1]];
-        } else if (!before && indexOfbucketToSwap < bucketsAsSortedArray.value.length - 1) {
-            [bucketsAsSortedArray.value[indexOfbucketToSwap + 1], bucketsAsSortedArray.value[indexOfbucketToSwap]] = [bucketsAsSortedArray.value[indexOfbucketToSwap], bucketsAsSortedArray.value[indexOfbucketToSwap + 1]];
-        }
-    }
-
-    return {
-        bucketsAsSortedArray,
-        bucketsAsMap,
-        getBucketNamesSorted,
-        updateBucketName,
-        deleteBucket,
-        addNewBucket,
-        swapWithBucket
-    };
+  return {
+    bucketsAsSortedArray,
+    bucketsAsMap,
+    getBucketNamesSorted,
+    updateBucketName,
+    deleteBucket,
+    addNewBucket,
+    swapWithBucket
+  };
 });
