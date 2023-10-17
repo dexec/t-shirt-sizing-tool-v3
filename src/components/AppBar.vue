df1725ef9994acdd59a3fcb8e9ca8a1124e7631d
Daniel Ackermann <d.ackermann@smf.de>
2023-10-17 13:34:17 +0200

    Farbanpassungen

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -1,17 +1,20 @@
 <template>
-  <v-app-bar color="blue" flat>
+  <v-app-bar color="#03787c" flat>
+<!--    TODO Icon einbauen-->
     <v-tabs>
-      <v-tab to="/projekt">Projektübersicht</v-tab>
-      <v-tab to="/pakete">Paketübersicht</v-tab>
-      <v-tab v-if="projektStore.bucketmodus" to="/vergleich">Vergleich</v-tab>
-      <v-tab to="/buckets">Bucketübersicht</v-tab>
-      <v-tab to="/kalkulation">Projektkalkulation</v-tab>
-      <v-tab to="/test">Test</v-tab>
-      <v-tab to="/test2">Test2</v-tab>
+      <v-tab style="color: white" to="/projekt">Projektübersicht</v-tab>
+      <v-tab style="color: white" to="/pakete">Paketübersicht</v-tab>
+      <v-tab style="color: white" v-if="projektStore.bucketmodus" to="/vergleich">Vergleich</v-tab>
+      <v-tab style="color: white" to="/buckets">Bucketübersicht</v-tab>
+      <v-tab style="color: white" to="/kalkulation">Projektkalkulation</v-tab>
+      <v-tab style="color: white" to="/test">Test</v-tab>
+      <v-tab style="color: white" to="/test2">Test2</v-tab>
     </v-tabs>
   </v-app-bar>
 </template>
+<style scoped>
 
+</style>
 <script setup>
   import {useProjektStore} from "@/stores/projekt";
   const projektStore = useProjektStore();
