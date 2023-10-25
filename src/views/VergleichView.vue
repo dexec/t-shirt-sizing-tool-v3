6cc8d077d333e13009b5aa846c269cdfcd7d40ed
Daniel Ackermann <d.ackermann@smf.de>
2023-10-25 17:31:56 +0200

    Es können nur noch so viele Buckets angezeigt werden wie sie in den Viewport passen

--- src/views/VergleichView.vue
+++ src/views/VergleichView.vue
@@ -1,19 +1,19 @@
 <template>
-  <v-container>
+  <v-container id="container">
     <v-row class="mb-6">
       <v-col class="d-flex flex-nowrap justify-center">
         <v-btn class="clickable-element bucketsButton" @click="showPaketeWithoutBucket=!showPaketeWithoutBucket"><span
-            class="bucketsButtonText">Toggle Arbeitspakete</span></v-btn>
+          class="bucketsButtonText">Toggle Arbeitspakete</span></v-btn>
         <v-dialog v-model="dialog" width="auto">
           <template v-slot:activator="{props}">
             <v-btn class="mx-6 bucketsButton clickable-element" v-bind="props">
               <span class="bucketsButtonText">Buckets konfigurieren</span>
             </v-btn>
           </template>
-
-          <v-card>
-            <v-card-text>
-              <v-checkbox v-for="bucket in buckets" :key="bucket.id" v-model="selected" :label=bucket.name
+          <v-card style="width: 10vw">
+            <v-card-text class="d-flex flex-column justify-center align-center">
+              <span>{{selected.length}}/{{numberBucketsToShow}}</span>
+              <v-checkbox v-for="bucket in buckets" :key="bucket.id" v-model="selected" :label=bucket.name :disabled="numberBucketsToShow==selected.length && !selected.includes(bucket.id)"
                           :value="bucket.id" @change="sortSelectedBuckets()">
               </v-checkbox>
               <div v-if="buckets.length == 0">Es gibt keine Buckets</div>
@@ -26,6 +26,7 @@
                    ghostClass="ghostClass"
                    group="pakete"
                    itemKey="name"
+                   style="margin: 0"
                    @change="removePaketFromBucket"
         >
           <template #header>
@@ -38,42 +39,40 @@
     </v-row>
     <v-row>
       <v-col :style="{visibility: (showPaketeWithoutBucket ? 'visible':'hidden')}" cols="3">
-        <v-text-field id="filterForPaketeList" v-model="paketeListeFilter" label="Pakete filtern" clearable @click:clear="paketeListeFilter=''"
-                      style="width:200px;"></v-text-field>
+        <v-text-field id="filterForPaketeList" v-model="paketeListeFilter" clearable label="Pakete filtern"
+                      style="width:200px;"
+                      @click:clear="paketeListeFilter=''"></v-text-field>
         <h2>Unzugewiesene Pakete</h2>
         <draggable
-            v-if="paketeWithoutBucket.length>0"
-            :list="paketeWithoutBucket"
-            :sort="false"
-            class="dragArea list-group"
-            ghostClass="ghostClass"
-            group="pakete"
-            itemKey="name"
-            style="overflow-y:auto;height: 70vh"
-            @change="removePaketFromBucket"
+          v-if="paketeWithoutBucket.length > 0"
+          :list="paketeWithoutBucket"
+          :sort="false"
+          class="dragArea list-group"
+          group="pakete"
+          itemKey="name"
+          style="overflow-y:auto;height: 70vh"
+          @change="removePaketFromBucket"
         >
           <template #item="{ element }">
-            <div v-if="applyFilter(element,paketeListeFilter)" :title="getTitleForPaket(element)" class="paket ma-2"
+            <div v-if="applyFilter(element,paketeListeFilter)" :title="getTitleForPaket(element)" class="paket"
                  style="position: relative;">
               <span class="paketContent">{{ (element as Paket).ticket_nr }}</span>
               <span class="paketContent">{{ (element as Paket).thema }}</span>
             </div>
           </template>
         </draggable>
-        <span v-if="paketeWithoutBucket.length==0">Es gibt keine Pakete ohne Bucket</span>
+        <span v-if="paketeWithoutBucket.length == 0">Es gibt keine Pakete ohne Bucket</span>
       </v-col>
       <v-col>
-        <v-row>
-          <v-col>
-            <v-text-field id="filterForPakete" v-model="paketeTabelleFilter" label="Pakete filtern" clearable @click:clear="paketeTabelleFilter=''"
-                          style="width:200px;"></v-text-field>
-          </v-col>
-        </v-row>
+        <v-text-field id="filterForPakete" v-model="paketeTabelleFilter" clearable label="Pakete filtern"
+                      style="width:200px;"
+                      @click:clear="paketeTabelleFilter=''"></v-text-field>
+        <h2>Buckettabelle</h2>
         <v-row>
           <v-col class="d-flex flex-nowrap justify-start">
             <div v-for="bucketId of selected" :key="bucketId">
               <div v-if="bucketsAsMap.get(bucketId)" class="list-group">
-                <div class="paket ma-2">{{ bucketsAsMap.get(bucketId).name }}</div>
+                <div class="paket">{{ bucketsAsMap.get(bucketId).name }}</div>
               </div>
             </div>
           </v-col>
@@ -81,23 +80,23 @@
         <v-row style="height:65vh; overflow-y:auto; overflow-x:hidden;">
           <v-col class="d-flex flex-nowrap justify-start">
             <draggable
-                v-for="bucketId of selected" :key="bucketId"
-                :list="getPaketeSortedByBucket(bucketsAsMap.get(bucketId)!)"
-                class="dragArea list-group"
-                group="pakete"
-                itemKey="name"
-                style="min-height: 100%;"
-                @change="changeBucketOfPaket($event,buckets.find(bucket => bucket.id == bucketId)!)"
+              v-for="bucketId of selected" :key="bucketId"
+              :list="getPaketeSortedByBucket(bucketsAsMap.get(bucketId)!)"
+              class="dragArea list-group"
+              group="pakete"
+              itemKey="name"
+              style="min-height: 100%;"
+              @change="changeBucketOfPaket($event,buckets.find(bucket => bucket.id == bucketId)!)"
             >
               <template #item="{ element }">
                 <div v-if="applyFilter(element, paketeTabelleFilter)" :title="getTitleForPaket(element)"
-                     class="paket ma-2">
-                  <span :style="searchPaket(element)" class="list-group-item paketContent">{{
-                      (element as Paket).ticket_nr
-                    }}</span>
-                  <span :style="searchPaket(element)" class="list-group-item  paketContent">{{
-                      (element as Paket).thema
-                    }}</span>
+                     class="paket">
+                  <span :style="searchPaket(element)" class="list-group-item paketContent">
+                    {{ (element as Paket).ticket_nr }}
+                  </span>
+                  <span :style="searchPaket(element)" class="list-group-item paketContent">
+                    {{ (element as Paket).thema }}
+                  </span>
                 </div>
               </template>
             </draggable>
@@ -109,63 +108,78 @@
 </template>
 
 <script lang="ts" setup>
-import {usePaketeStore} from "@/stores/pakete";
-import {useBucketsStore} from "@/stores/buckets";
-import {computed, onActivated, ref} from "vue";
-import {Bucket} from "@/models/Bucket";
-import {Paket} from "@/models/Paket";
+import { usePaketeStore } from "@/stores/pakete";
+import { useBucketsStore } from "@/stores/buckets";
+import { computed, onActivated, ref } from "vue";
+import { Bucket } from "@/models/Bucket";
+import { Paket } from "@/models/Paket";
 import draggable from "vuedraggable";
-import "@/styles/hoveLink.css"
+import "@/styles/hoveLink.css";
 
 const paketeStore = usePaketeStore();
 const bucketStore = useBucketsStore();
 const dialog = ref(false);
 const unsortedPaketeListsSortedByBucketsMap = paketeStore.unsortedPaketeListsSortedByBucketsMap;
-const showPaketeWithoutBucket = ref(true)
+const showPaketeWithoutBucket = ref(true);
 const searchedPaket = ref(0);
-const paketeWithoutBucket = computed(() => paketeStore.paketeChildrenWithNoBucket())
+const paketeWithoutBucket = computed(() => paketeStore.paketeChildrenWithNoBucket());
 const buckets = bucketStore.bucketsAsSortedArray as Bucket[];
 const bucketsAsMap = bucketStore.bucketsAsMap;
+window.addEventListener('resize', function () {
+  dialog.value=false
+  setNumberBucketsToShow()
+});
+const numberBucketsToShow = ref(4);
 const selected = ref<number[]>(buckets.map(bucket => bucket.id));
 onActivated(() => {
+  setNumberBucketsToShow()
   sortSelectedBuckets();
 });
-
+function setNumberBucketsToShow() {
+  const clientWidth = document.getElementById("container")?.clientWidth;
+  if (clientWidth != null) {
+    const bucketTableWidth = clientWidth * 0.75;
+    numberBucketsToShow.value = Math.trunc(bucketTableWidth / 200);
+    while (selected.value.length > numberBucketsToShow.value) {
+      selected.value.pop()
+    }
+  }
+}
 function getTitleForPaket(paket: Paket): string {
   let result: string = "";
   const parents = paketeStore.parentsOfPaket(paket).reverse();
   for (const paket of parents) {
-    result += paket.thema + "\n";
+    result += paket.ticket_nr + " " + paket.thema + "\n";
     for (let i = 0; i < paket.lvl + 1; i++) {
-      result += "\t"
+      result += "\t";
     }
   }
-  result += paket.thema
-  return result
+  result += paket.ticket_nr + " " + paket.thema;
+  return result;
 }
 
 const paketeTabelleFilter = ref("");
 const paketeListeFilter = ref("");
 
 function applyFilter(paket: Paket, filterString: string): boolean {
-  const paketStringIndexed: { [index: string]: any } = paket
-    for (const key in paketStringIndexed) {
-      if (typeof paketStringIndexed[key] === "string" && paketStringIndexed[key].toUpperCase().includes(filterString.toUpperCase())) {
-        return true
-      }
+  const paketStringIndexed: { [index: string]: any } = paket;
+  for (const key in paketStringIndexed) {
+    if (typeof paketStringIndexed[key] === "string" && paketStringIndexed[key].toUpperCase().includes(filterString.toUpperCase())) {
+      return true;
     }
-  return false
+  }
+  return false;
 }
 
 function sortSelectedBuckets() {
-  const copySelected = [...selected.value]
-  selected.value.length = 0
+  const copySelected = [...selected.value];
+  selected.value.length = 0;
   for (let bucket of buckets) {
     if (copySelected.includes(bucket.id)) {
-      selected.value.push(bucket.id)
+      selected.value.push(bucket.id);
     }
   }
-  copySelected.length = 0
+  copySelected.length = 0;
 }
 
 /*function getPaketSortedByBucket(bucket: Bucket) {
@@ -237,8 +251,8 @@ function onEndDrag(e: any) {
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  width: 150px; /* Adjust the width as needed */
-  height: 100px; /* Adjust the height as needed */
+  width: 150px;
+  height: 100px;
 }
 
 .bucketsButtonText {
@@ -252,10 +266,11 @@ function onEndDrag(e: any) {
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  width: 150px; /* Adjust the width as needed */
-  height: 100px; /* Adjust the height as needed */
+  width: 150px;
+  height: 100px;
   min-height: 100px;
-  border: 1px solid #000; /* Optional: Add a border for the box */
+  border: 1px solid #000;
+  margin: 25px;
 }
 
 .paketContent {
