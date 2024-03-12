18d1b67e61a4e98240aa453a779ec105cabe50fe
Daniel Ackermann <d.ackermann@smf.de>
2024-03-12 13:58:25 +0100

    Refactor

--- src/enums/ColumnET.ts
+++ src/enums/ColumnET.ts
@@ -1,7 +1,7 @@
 export const enum ColumnET {
-  BEZEICHNUNG= "bezeichnung",
-  AUFSCHLAG= "aufwandRelativ",
-  AUFWAND= "aufwandAbsolut",
-  ZWISCHENSUMME= "anteilZwischensumme",
-  GESAMTPROJEKT= "anteilGesamtprojekt"
+    BEZEICHNUNG = "bezeichnung",
+    AUFSCHLAG = "aufwandRelativ",
+    AUFWAND = "aufwandAbsolut",
+    ZWISCHENSUMME = "anteilZwischensumme",
+    GESAMTPROJEKT = "anteilGesamtprojekt"
 }
