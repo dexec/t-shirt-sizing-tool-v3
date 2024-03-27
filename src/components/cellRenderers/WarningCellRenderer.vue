ce104144c513071cf51d018cdcbd8ec4d1f80d21
Daniel Ackermann <d.ackermann@smf.de>
2024-03-27 18:00:45 +0100

    progress

--- src/components/cellRenderers/WarningCellRenderer.vue
+++ src/components/cellRenderers/WarningCellRenderer.vue
@@ -1,10 +1,16 @@
 <template>
-  <v-icon icon="mdi-alert "></v-icon>
+  <div v-if="rowsUpdated"><v-icon icon="mdi-alert "></v-icon></div>
+  <div v-else>abc</div>
 </template>
 
 <script setup lang="ts">
-  const props = defineProps(['params']);
-  console.log(props.params.gridApi);
+const props = defineProps(['params']);
+  let rowsUpdated:boolean = false;
+    let count = 0;
+    props.params.api.forEachNode(() => {
+      count++;
+    })
+    rowsUpdated = count >=5;
 </script>
 
 <style scoped>
