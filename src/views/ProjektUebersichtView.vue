593df0d9a15bb7b8f2029c207aa829e031e45371
Daniel Ackermann <d.ackermann@smf.de>
2024-04-09 12:31:20 +0200

    TODO erstellt

--- src/views/ProjektUebersichtView.vue
+++ src/views/ProjektUebersichtView.vue
@@ -39,6 +39,7 @@
                   {{ bucket.name }}
                 </div>
                 <div v-else>
+<!--                  TODO Eingabe limitieren, zu viele Buchstaben machen das Rechteck kaputt...-->
                   <input class="bucketEdit" id="currentEditBucket" v-model="newBucketName"
                          style="width: 80px;text-align: center; line-height: 80px; height: 80px;"
                          type="text"
