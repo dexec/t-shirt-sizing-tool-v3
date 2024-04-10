b3e6285479605f6be6ca4d0cb327f523a1b532c7
Daniel Ackermann <d.ackermann@smf.de>
2024-04-10 17:24:11 +0200

    Warnungen für fehlerhafte Schätzungen eingebaut

--- src/components/cellRenderers/WarningCellRenderer.vue
+++ src/components/cellRenderers/WarningCellRenderer.vue
@@ -1,16 +1,30 @@
 <template>
-  <div v-if="rowsUpdated"><v-icon icon="mdi-alert "></v-icon></div>
-  <div v-else>abc</div>
+  <div v-if="showWarning">
+    <v-icon icon="mdi-alert "></v-icon>
+  </div>
 </template>
 
-<script setup lang="ts">
-const props = defineProps(['params']);
-  let rowsUpdated:boolean = false;
-    let count = 0;
-    props.params.api.forEachNode(() => {
-      count++;
-    })
-    rowsUpdated = count >=5;
+<script lang="ts" setup>
+import { useStatistikenStore } from "@/stores/statistiken";
+import type { Paket } from "@/models/Paket";
+
+const statistikenStore = useStatistikenStore();
+statistikenStore.berechne();
+const props = defineProps(["params"]);
+const paket = props.params.data as Paket;
+let showWarning: boolean = false;
+if (paket.bucket != null && paket.schaetzung != null && paket.zurRechnungFreigegeben()) {
+  for (const statistik of statistikenStore.statistiken) {
+    if(statistik.bucket.id == paket.bucket.id) continue
+    const min = statistik.min;
+    const max = statistik.max;
+    if (min == null || max == null) continue;
+    if (paket.schaetzung >= min && paket.schaetzung <= max) {
+      showWarning = true;
+      break;
+    }
+  }
+}
 </script>
 
 <style scoped>
