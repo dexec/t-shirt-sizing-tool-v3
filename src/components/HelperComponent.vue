c075321f96dbf8a348d178885ba7318f69c42e37
Daniel Ackermann <d.ackermann@smf.de>
2024-04-17 17:44:22 +0200

    Neue Komponente

---
+++ src/components/HelperComponent.vue
@@ -0,0 +1,24 @@
+
+<template>
+  <div id="container">
+    <div v-for="entry of entriesProp" :key="entriesProp.indexOf(entry)">
+      {{entry}}
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps(["entriesProp"]);
+</script>
+
+
+<style scoped>
+#container {
+  position: fixed;
+  height: 300px;
+  top: 100px;
+  right: 20px;
+  background-color: #8ee774;
+  z-index: 5;
+}
+</style>
