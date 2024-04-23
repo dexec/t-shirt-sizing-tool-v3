6eaa30dc24a21aa7655db438145971e614338979
Daniel Ackermann <d.ackermann@smf.de>
2024-04-23 09:09:16 +0200

    Store für Projektkalkulation erstellt

---
+++ src/stores/projektkalkulation.ts
@@ -0,0 +1,37 @@
+import {defineStore} from "pinia";
+import {ref} from "vue";
+
+export const useProjektkalkulationStore = defineStore("projektkalkulation", () => {
+
+    const currentSelectedRow = ref(-1);
+    const currentSelectedColumn = ref("")
+    const erklaerungsText = ref("");
+    const erklaerungsTextZusatz = ref("");
+    const erklaerungsRechnung = ref("");
+    const erklaerungsRechnungZusatz = ref("");
+    const colorCells = ref(false);
+    const explain = ref(false);
+    function getFullText(): string {
+        return erklaerungsRechnung.value + "\n" +  erklaerungsRechnungZusatz.value
+    }
+
+    function clearErklaerungen() {
+        erklaerungsText.value = "";
+        erklaerungsTextZusatz.value = "";
+        erklaerungsRechnung.value = "";
+        erklaerungsRechnungZusatz.value = "";
+    }
+
+    return {
+        currentSelectedRow,
+        currentSelectedColumn,
+        erklaerungsText,
+        erklaerungsTextZusatz,
+        erklaerungsRechnung,
+        erklaerungsRechnungZusatz,
+        colorCells,
+        explain,
+        clearErklaerungen,
+        getFullText
+    };
+});
