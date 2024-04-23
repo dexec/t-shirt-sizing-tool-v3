038fd299a004fd9a7f570b05c5fafd6a0db14195
Daniel Ackermann <d.ackermann@smf.de>
2024-04-23 09:09:32 +0200

    Testing

--- src/views/testView.vue
+++ src/views/testView.vue
@@ -1,12 +1,20 @@
 <template>
-  <div @keyup.enter="console.log('entered')">
-    <test-comp>
-
-    </test-comp>
-  </div>
+  <v-tooltip v-model="isActive">
+    <template v-slot:activator="{props}">
+      <v-icon v-bind="props" @mouseenter="isHovered=true" @mouseleave="isHovered=false"  icon="mdi-help"></v-icon>
+    </template>
+  </v-tooltip>
+  <v-btn @click="isActive=!isActive"></v-btn>
+  {{isHovered}}
 </template>
 
-<script setup lang="ts">
+<script lang="ts" setup>
+import {ref, watch} from "vue";
+const isActive = ref(false)
+const isHovered = ref(false)
 
-import TestComp from "@/components/testComp.vue";
+ watch( isHovered, (newState) => {
+   console.log(newState);
+ })
+
 </script>
